77eab81ecea2ee6943765197a1db9a5d4d1d43c8
Steve Sewell <steve@builder.io>
2020-11-05 17:18:20 -0800

    jsx lite

--- packages/login-form/src/LoginForm.story.tsx
+++
@@ -1,9 +0,0 @@
-import { action } from '@storybook/addon-actions';
-import { storiesOf } from '@storybook/react';
-import * as React from 'react';
-import { LoginForm } from './LoginForm';
-
-storiesOf('LoginForm', module).add('default', () => {
-  const onClick = action('logIn clicked');
-  return <LoginForm onClick={onClick} />;
-});
