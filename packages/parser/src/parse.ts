60a049820b9e5a85b5e99433f5f016951ba1ee38
Steve Sewell <steve@builder.io>
2020-11-06 16:17:03 -0800

    lint

--- packages/parser/src/parse.ts
+++ packages/parser/src/parse.ts
@@ -1,1 +1,19 @@
-export function parse(jsx: string) {}
+type JSXLiteNode = {
+  '@type': '@jsx-lite/node';
+  children: JSXLiteNode[];
+};
+
+type JSONPrimitive = string | null | number | boolean;
+type JSONObject = { [key: string]: JSON };
+type JSON = JSONPrimitive | JSONObject | JSON[];
+
+type JSXLiteComponent = {
+  '@type': '@jsx-lite/component';
+  state: { [key: string]: JSON };
+  children: JSXLiteNode[];
+};
+
+export function parse(jsx: string): JSXLiteComponent {
+  // TODO
+  return null as any;
+}
