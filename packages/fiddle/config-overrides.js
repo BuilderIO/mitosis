f206c836bd306fc75ab1543ecfd1136b99d696e4
Steve Sewell <steve@builder.io>
2020-11-18 16:47:01 -0800

    get build working

--- packages/fiddle/config-overrides.js
+++ packages/fiddle/config-overrides.js
@@ -4,8 +4,16 @@ const {
   addWebpackPlugin,
 } = require('customize-cra');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const os = require('os');
 
-module.exports = override(
-  addBabelPresets('@emotion/babel-preset-css-prop'),
-  addWebpackPlugin(new MonacoWebpackPlugin()),
-);
+module.exports = function(config, env) {
+  const conf = override(
+    addBabelPresets('@emotion/babel-preset-css-prop'),
+    addWebpackPlugin(new MonacoWebpackPlugin()),
+  )(config, env);
+
+  // For now turn minimize off as it's blowing out memory so builds never succeed
+  conf.optimization.minimize = false;
+
+  return conf;
+};
