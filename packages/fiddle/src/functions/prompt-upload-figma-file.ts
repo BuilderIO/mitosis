07cba13fba4ca4cf750ecb58149da7053b03b49c
Steve Sewell <steve@builder.io>
2020-11-23 12:25:11 -0800

    hide temp upload input

--- packages/fiddle/src/functions/prompt-upload-figma-file.ts
+++ packages/fiddle/src/functions/prompt-upload-figma-file.ts
@@ -3,6 +3,7 @@ import { BuilderContent } from '@builder.io/sdk';
 export async function promptUploadFigmaJsonFile(): Promise<BuilderContent> {
   return new Promise((resolve, reject) => {
     const input = document.createElement('input');
+    input.style.display = 'none';
     input.type = 'file';
     document.body.appendChild(input);
     input.click();
