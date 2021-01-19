43aae3aa505207777c95801d4c1d5fe2eea3bf40
Steve Sewell <steve@builder.io>
2021-01-19 10:44:23 -0800

    prettier

--- packages/core/src/generators/html.ts
+++ packages/core/src/generators/html.ts
@@ -44,7 +44,7 @@ const addUpdateAfterSet = (
   json: JSXLiteComponent,
   options: InternalToHtmlOptions,
 ) => {
-  traverse(json).forEach(function (item) {
+  traverse(json).forEach(function(item) {
     if (isJsxLiteNode(item)) {
       for (const key in item.bindings) {
         const value = item.bindings[key] as string;
@@ -60,7 +60,7 @@ const addUpdateAfterSet = (
 
 const getForNames = (json: JSXLiteComponent) => {
   const names: string[] = [];
-  traverse(json).forEach(function (item) {
+  traverse(json).forEach(function(item) {
     if (isJsxLiteNode(item)) {
       if (item.name === 'For') {
         names.push(item.bindings._forName as string);
