ccf11f901435f03780c8e23b1b5237c028be998b
Steve Sewell <steve@builder.io>
2021-01-20 14:06:43 -0800

    fix builder tests

--- packages/core/src/__tests__/builder.test.ts
+++ packages/core/src/__tests__/builder.test.ts
@@ -3,15 +3,15 @@ import { componentToJsxLite } from '../generators/jsx-lite';
 import { builderContentToJsxLiteComponent } from '../parsers/builder';
 import { parseJsx } from '../parsers/jsx';
 
-const basic = require('./data/blocks/stamped-io.raw');
+const stamped = require('./data/blocks/stamped-io.raw');
 
 describe('Builder', () => {
   test('Stamped', () => {
-    const json = parseJsx(basic);
+    const json = parseJsx(stamped);
     const builderJson = componentToBuilder(json);
     expect(builderJson).toMatchSnapshot();
 
-    const backToJsxLite = builderContentToJsxLiteComponent(json);
+    const backToJsxLite = builderContentToJsxLiteComponent(builderJson);
     const jsxLite = componentToJsxLite(backToJsxLite);
     expect(jsxLite).toMatchSnapshot();
   });
