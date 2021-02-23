5599da3f20063dd79eae07aeac56e9a339dff700
Dylan Kendal <dylankendal@gmail.com>
2021-02-23 14:12:26 -0500

    Add help docs to jsx-lite cli

--- packages/cli/readme.md
+++ packages/cli/readme.md
@@ -1,6 +1,26 @@
-# jsx-lite CLI
+# JSX-Lite CLI
 
 A CLI for jsx-lite.
+
+## Installation
+
+**Manual installation**
+```bash
+git clone git@github.com:BuilderIO/jsx-lite.git
+cd jsx-lite/packages/cli
+npm install
+npm run build
+npm link
+```
+
+## Usage
+
+```bash
+jsx-lite --to=<format> < my-file.tsx
+cat my-file.tsx | jsx-lite
+```
+
+Check the output from `jsx-lite --help`.
 
 ## Customizing your CLI
 
--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,4 +1,41 @@
-const { build } = require('gluegun')
+import { build } from 'gluegun'
+import { Toolbox } from 'gluegun/build/types/domain/toolbox'
+
+
+const help = (toolbox: Toolbox) =>
+  toolbox.print.info(
+    `
+jsx-lite command line component processor [version ${toolbox.meta.version()}]
+
+USAGE
+	jsx-lite --to=<format>
+	jsx-lite -t=<format>
+
+	Input is read from standard in.
+
+EXAMPLES
+	jsx-lite -t react < component.tsx
+	cat component.tsx | jsx-lite -t html
+
+OPTIONS
+	--to=<format>,	-t=<format>
+			Specify output format. <format> can be one of:
+
+			- reactNative
+			- solid
+			- vue
+			- react
+			- template
+			- html
+			- customElement
+			- jsxLite
+			- builder
+			- swift
+			- svelte
+			- liquid
+			- angular
+`.trim()
+  )
 
 /**
  * Create the cli and kick it off
@@ -9,12 +46,22 @@ async function run(argv: any) {
     .brand('jsx-lite')
     .src(__dirname)
     .plugins('./node_modules', { matching: 'jsx-lite-*', hidden: true })
-    .help() // provides default for help, h, --help, -h
+    .help(help) // provides default for help, h, --help, -h
     .version() // provides default for version, v, --version, -v
+    // enable the following method if you'd like to skip loading one of these core extensions
+    // this can improve performance if they're not necessary for your project:
+    .exclude([
+      // 'meta',
+      // 'print',
+      // 'filesystem',
+      // 'semver',
+      // 'system',
+      'http',
+      'template',
+      'patching',
+      'package-manager'
+    ])
     .create()
-  // enable the following method if you'd like to skip loading one of these core extensions
-  // this can improve performance if they're not necessary for your project:
-  // .exclude(['meta', 'strings', 'print', 'filesystem', 'semver', 'system', 'prompt', 'http', 'template', 'patching', 'package-manager'])
   // and run it
   const toolbox = await cli.run(argv)
 
