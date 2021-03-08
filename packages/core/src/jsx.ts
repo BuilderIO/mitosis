465a3d6cc004a9cd702b5f7d48bcc4b7db6a77cc
Steve Sewell <steve@builder.io>
2021-03-08 12:47:14 -0800

    prettier and test snapshots

--- packages/core/src/jsx.ts
+++ packages/core/src/jsx.ts
@@ -1204,9 +1204,8 @@ declare global {
           'color-interpolation' | 'color-rendering'
         > {}
 
-    interface AnimateMotionSVGAttributes<
-      T
-    > extends AnimationElementSVGAttributes<T>,
+    interface AnimateMotionSVGAttributes<T>
+      extends AnimationElementSVGAttributes<T>,
         // XLinkSVGAttributes,
         AnimationTimingSVGAttributes,
         AnimationValueSVGAttributes,
@@ -1217,9 +1216,8 @@ declare global {
       origin?: 'default';
     }
 
-    interface AnimateTransformSVGAttributes<
-      T
-    > extends AnimationElementSVGAttributes<T>,
+    interface AnimateTransformSVGAttributes<T>
+      extends AnimationElementSVGAttributes<T>,
         // XLinkSVGAttributes,
         AnimationAttributeTargetSVGAttributes,
         AnimationTimingSVGAttributes,
@@ -1369,9 +1367,8 @@ declare global {
       stdDeviation?: number | string;
     }
 
-    interface FeImageSVGAttributes<
-      T
-    > extends FilterPrimitiveElementSVGAttributes<T>,
+    interface FeImageSVGAttributes<T>
+      extends FilterPrimitiveElementSVGAttributes<T>,
         // XLinkSVGAttributes,
         ExternalResourceSVGAttributes,
         StylableSVGAttributes {
