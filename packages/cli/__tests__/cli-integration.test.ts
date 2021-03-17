ef4e3b8359bbfb5acd1df955c97e4d09188e5039
Dylan Kendal <dylankendal@gmail.com>
2021-03-17 18:34:42 -0400

    Fix broken test

--- packages/cli/__tests__/cli-integration.test.ts
+++ packages/cli/__tests__/cli-integration.test.ts
@@ -1,17 +1,18 @@
-import { system, filesystem } from 'gluegun'
+import { filesystem, system } from 'gluegun'
 
-const src = filesystem.path(__dirname, '..')
+const { version } = require('../package.json')
 
-const script = filesystem.path(src, 'bin', 'jsx-lite')
+const root = filesystem.path(__dirname, '..')
+const script = filesystem.path(root, 'bin', 'jsx-lite')
 
 const cli = async (cmd: string) => system.run(`node ${script} ${cmd}`)
 
 test('outputs version', async () => {
   const output = await cli('--version')
-  expect(output).toContain('0.0.1')
+  expect(output).toContain(version)
 })
 
 test('outputs help', async () => {
   const output = await cli('--help')
-  expect(output).toContain('0.0.1')
+  expect(output).toContain(version)
 })
