2f815a40928c21e8c6b0e92c99b48106d98ce923
Dylan Kendal <dylankendal@gmail.com>
2021-03-19 16:51:48 -0400

    Add opts to test parser

--- packages/eslint-plugin/src/rules/__tests__/jsx-callback-arg-name.ts
+++ packages/eslint-plugin/src/rules/__tests__/jsx-callback-arg-name.ts
@@ -1,15 +1,28 @@
 import { RuleTester } from 'eslint'
 import rule from '../jsx-callback-arg-name'
 
+const opts = {
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+} as const
+
 var ruleTester = new RuleTester()
+
 ruleTester.run('jsx-callback-arg-name', rule, {
   valid: [
     // give me some code that won't trigger a warning
+    { ...opts, code: '<button onClick={ event => console.log(event) }/>' },
   ],
 
   invalid: [
     {
-      code: '20c4f03',
+      ...opts,
+      code: '<button onClick={ e => console.log(e) }/>',
       errors: [{
         message: 'Fill me in.',
         type: 'Me too',
