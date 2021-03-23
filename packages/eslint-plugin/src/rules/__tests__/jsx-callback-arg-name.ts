823abf6f86dfd317031b17cb382d4b2c8ade6764
Dylan Kendal <dylankendal@gmail.com>
2021-03-23 19:28:41 -0400

    Add jsx-callback-arg-name rule
    Adds a rule and fixer for callback's arguments - first argument must be
    named `event`.

--- packages/eslint-plugin/src/rules/__tests__/jsx-callback-arg-name.ts
+++ packages/eslint-plugin/src/rules/__tests__/jsx-callback-arg-name.ts
@@ -15,18 +15,36 @@ var ruleTester = new RuleTester()
 
 ruleTester.run('jsx-callback-arg-name', rule, {
   valid: [
-    // give me some code that won't trigger a warning
-    { ...opts, code: '<button onClick={ event => console.log(event) }/>' },
+    { code: '<button/>', ...opts },
+    { code: '<button type="button"/>', ...opts },
+    { code: '<button onClick={ null }/>', ...opts },
+    { code: '<button onClick={ "string" }/>', ...opts },
+    { code: '<button onClick={ event => doSomething(event) }/>', ...opts },
+    { code: '<button onClick={ () => doSomething() }/>', ...opts },
+    { code: '<button onClick={ function(event) {} }/>', ...opts },
   ],
 
   invalid: [
     {
-      ...opts,
+      // We make no attempt to rename all references to the variable
       code: '<button onClick={ e => console.log(e) }/>',
-      errors: [{
-        message: 'Fill me in.',
-        type: 'Me too',
-      }],
+      output: '<button onClick={ event => console.log(e) }/>',
+      errors: ['Callback parameter must be called `event`'],
+      ...opts,
+    },
+    // Arrow and normal functions are handled
+    {
+      code: '<button onClick={ function (e) {} }/>',
+      output: '<button onClick={ function (event) {} }/>',
+      errors: ['Callback parameter must be called `event`'],
+      ...opts,
+    },
+    // Renames the variable
+    {
+      code: '<button onClick={ function foobar(e) {} }/>',
+      output: '<button onClick={ function foobar(event) {} }/>',
+      errors: ['Callback parameter must be called `event`'],
+      ...opts,
     },
   ],
 })
--- packages/eslint-plugin/src/rules/jsx-callback-arg-name.ts
+++ packages/eslint-plugin/src/rules/jsx-callback-arg-name.ts
@@ -1,16 +1,78 @@
 import type { Rule } from 'eslint'
+import type * as ESTree from 'estree'
+
+interface NodeWithParent extends ESTree.BaseNode {
+  parent: NodeWithParent
+  type: NodeType
+}
+
+interface JSXExpressionContainer extends NodeWithParent {
+  type: 'JSXExpressionContainer'
+  expression: NodeExt
+}
+
+interface JSXAttribute extends NodeWithParent {
+  type: 'JSXAttribute'
+  value: JSXExpressionContainer
+}
+
+interface JSXOpeningElement extends NodeWithParent {
+  type: 'JSXOpeningElement'
+  attributes: JSXAttribute[]
+}
+
+declare module 'eslint' {
+  export namespace Rule {
+    interface NodeListener {
+      JSXOpeningElement?(node: JSXOpeningElement): void
+      JSXAttribute?(node: JSXAttribute): void
+      JSXExpressionContainer?(node: JSXExpressionContainer): void
+    }
+  }
+}
+
+type NodeJSX = JSXExpressionContainer | JSXAttribute | JSXOpeningElement
+type NodeExt = ESTree.Node | NodeJSX
+type NodeType = NodeExt['type']
+
+/**
+ * Helper function to make defining type guards more sane.
+ * Supply a function that returns a value or false, will infer the guard's type
+ * from the union of types returned.
+ */
+function defguard<B extends A, A>(fn: (term: A) => B | false) {
+  return function(term: A): term is B {
+    return fn(term) !== false
+  }
+}
+
+const isFunction = defguard((term: NodeExt) => {
+  const members = [
+    'FunctionDeclaration',
+    'FunctionExpression',
+    'ArrowFunctionExpression',
+  ] as const
+
+  for (const value of members) {
+    if (term.type === value) return term
+  }
+
+  return false
+})
+
 // ------------------------------------------------------------------------------
 // Rule Definition
 // ------------------------------------------------------------------------------
 
 const rule: Rule.RuleModule = {
   meta: {
+    type: 'problem',
     docs: {
       description: 'jsx-callback-arg-name',
       category: 'Fill me in',
       recommended: true,
     },
-    // fixable: null, // or "code" or "whitespace"
+    fixable: 'code',
     schema: [
       // fill in your schema
     ],
@@ -30,7 +92,36 @@ const rule: Rule.RuleModule = {
     // ----------------------------------------------------------------------
 
     const listener: Rule.RuleListener = {
-      // give me methods
+      JSXExpressionContainer(node) {
+        // Only focus on expressions that are attribute's values.
+        if (!(node.parent.type === 'JSXAttribute')) return
+
+        if (!isFunction(node.expression)) return
+
+        const params = node.expression.params
+
+        // No arguments is fine
+        if (params && params.length < 1) return
+
+        const [arg1] = params
+
+        if (arg1.type !== 'Identifier') {
+          return context.report({
+            node: arg1,
+            message: 'Must be a function parameter',
+          })
+        }
+
+        if (arg1.name !== 'event') {
+          return context.report({
+            node: arg1,
+            message: 'Callback parameter must be called `event`',
+            fix(fixer) {
+              return fixer.replaceText(arg1, 'event')
+            },
+          })
+        }
+      },
     }
 
     return listener
