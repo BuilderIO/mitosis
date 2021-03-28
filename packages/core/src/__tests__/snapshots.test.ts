692e68447c2560804c5e23808c13cdc1f7173dce
Luis <luis.lpv.22@gmail.com>
2021-03-29 01:17:26 +0200

    Fix suggestions

--- packages/core/src/__tests__/snapshots.test.ts
+++ packages/core/src/__tests__/snapshots.test.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import { parseJsx } from '../parsers/jsx';
 import { builderContentToJsxLiteComponent } from '../parsers/builder';
 import { componentToBuilder } from '../generators/builder';
@@ -9,7 +11,6 @@ import { componentToSolid } from '../generators/solid';
 import { componentToVue } from '../generators/vue';
 import { TestDataLoader, TestData } from './loaders/test-data-loader';
 
-import path from 'path';
 describe('Snapshot tests', () => {
   const dataLoader = new TestDataLoader({ subdirectories: true });
   const testCases = dataLoader.load(path.resolve(__dirname, './data'));
@@ -17,74 +18,75 @@ describe('Snapshot tests', () => {
   describe('Builder', () => {
     testCases.forEach((data: TestData) => {
       test(data.name, () => {
-      const jsx: string = require(data.file);
-      const json = parseJsx(jsx);
-      const builderJson = componentToBuilder(json);
-      expect(builderJson).toMatchSnapshot();
+        const jsx = fs.readFileSync(data.file, 'utf8');
+        const json = parseJsx(jsx);
+        const builderJson = componentToBuilder(json);
+        expect(builderJson).toMatchSnapshot();
 
-      const backToJsxLite = builderContentToJsxLiteComponent(builderJson);
-      const jsxLite = componentToJsxLite(backToJsxLite);
-      expect(jsxLite).toMatchSnapshot();
+        const backToJsxLite = builderContentToJsxLiteComponent(builderJson);
+        const jsxLite = componentToJsxLite(backToJsxLite);
+        expect(jsxLite).toMatchSnapshot();
+      });
     });
   });
-});
 
-describe('Html', () => {
-  testCases.forEach((data: TestData) => {
-    test(data.name, () => {
-      const jsx = fs.readFileSync(data.file, 'utf8');
-      const json = parseJsx(jsx);
-      const output = componentToHtml(json);
+  describe('Html', () => {
+    testCases.forEach((data: TestData) => {
+      test(data.name, () => {
+        const jsx = fs.readFileSync(data.file, 'utf8');
+        const json = parseJsx(jsx);
+        const output = componentToHtml(json);
 
-      expect(output).toMatchSnapshot();
+        expect(output).toMatchSnapshot();
+      });
     });
   });
-});
 
-describe('Liquid', () => {
-  testCases.forEach((data: TestData) => {
-    test(data.name, () => {
-      const jsx: string = require(data.file);
-      const json = parseJsx(jsx);
-      const output = componentToLiquid(json);
+  describe('Liquid', () => {
+    testCases.forEach((data: TestData) => {
+      test(data.name, () => {
+        const jsx = fs.readFileSync(data.file, 'utf8');
+        const json = parseJsx(jsx);
+        const output = componentToLiquid(json);
 
-      expect(output).toMatchSnapshot();
+        expect(output).toMatchSnapshot();
+      });
     });
   });
-});
 
-describe('React', () => {
-  testCases.forEach((data: TestData) => {
-    test(data.name, () => {
-      const jsx: string = require(data.file);
-      const json = parseJsx(jsx);
-      const output = componentToReact(json);
+  describe('React', () => {
+    testCases.forEach((data: TestData) => {
+      test(data.name, () => {
+        const jsx = fs.readFileSync(data.file, 'utf8');
+        const json = parseJsx(jsx);
+        const output = componentToReact(json);
 
-      expect(output).toMatchSnapshot();
+        expect(output).toMatchSnapshot();
+      });
     });
   });
-});
 
-describe('Solid', () => {
-  testCases.forEach((data: TestData) => {
-    test(data.name, () => {
-      const jsx: string = require(data.file);
-      const json = parseJsx(jsx);
-      const output = componentToSolid(json);
+  describe('Solid', () => {
+    testCases.forEach((data: TestData) => {
+      test(data.name, () => {
+        const jsx = fs.readFileSync(data.file, 'utf8');
+        const json = parseJsx(jsx);
+        const output = componentToSolid(json);
 
-      expect(output).toMatchSnapshot();
+        expect(output).toMatchSnapshot();
+      });
     });
   });
-});
 
-describe('Vue', () => {
-  testCases.forEach((data: TestData) => {
-    test(data.name, () => {
-      const jsx: string = require(data.file);
-      const json = parseJsx(jsx);
-      const output = componentToVue(json);
+  describe('Vue', () => {
+    testCases.forEach((data: TestData) => {
+      test(data.name, () => {
+        const jsx = fs.readFileSync(data.file, 'utf8');
+        const json = parseJsx(jsx);
+        const output = componentToVue(json);
 
-      expect(output).toMatchSnapshot();
+        expect(output).toMatchSnapshot();
+      });
     });
   });
 });
