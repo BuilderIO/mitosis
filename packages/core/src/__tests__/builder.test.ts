0aee2dd96479d60b0b39b9d7a615332c317e66b8
Dylan Kendal <dylankendal@gmail.com>
2021-04-20 10:54:36 -0400

    fix(cli): remove dead code

--- packages/core/src/__tests__/builder.test.ts
+++ packages/core/src/__tests__/builder.test.ts
@@ -30,7 +30,6 @@ describe('Builder', () => {
       state: { foo: 'bar' },
     });
 
-    const code2 = `Object.assign(state, { foo: 'bar' }); alert('hi');`;
     expect(extractStateHook(code)).toEqual({
       code: `alert('hi');`,
       state: { foo: 'bar' },
