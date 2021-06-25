5e792a1b882474b42894fed6db1726a38ba3cbec
Steve Sewell <steve@builder.io>
2021-06-25 07:59:11 -0700

    dev release and check useEffect must have an empty array as the second arg

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -9,7 +9,7 @@ export const staticControlFlow: Rule.RuleModule = {
     if (!isJSXLitePath(context.getFilename())) return {};
 
     return {
-      VariableDeclarator(node: any) {
+      VariableDeclarator(node) {
         if (types.isVariableDeclarator(node)) {
           if (
             types.isObjectPattern(node.id) &&
@@ -25,7 +25,32 @@ export const staticControlFlow: Rule.RuleModule = {
         }
       },
 
-      JSXExpressionContainer(node: any) {
+      CallExpression(node) {
+        if (types.isCallExpression(node)) {
+          if (
+            types.isIdentifier(node.callee) &&
+            node.callee.name === 'useEffect'
+          ) {
+            const useEffectMessage =
+              'Only useEffect with an empty array second argument is allowed. E.g. useEfffect(...) must be useEffect(..., [])';
+            const secondArg = node.arguments[1];
+            if (
+              !(
+                secondArg &&
+                types.isArrayExpression(secondArg) &&
+                secondArg.elements.length === 0
+              )
+            ) {
+              context.report({
+                node: node,
+                message: useEffectMessage,
+              });
+            }
+          }
+        }
+      },
+
+      JSXExpressionContainer(node) {
         if (types.isJSXExpressionContainer(node)) {
           if (types.isConditionalExpression(node.expression)) {
             if (
