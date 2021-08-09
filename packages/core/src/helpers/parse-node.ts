cc89a6649cacad5fac1340769585efd93b7e43c0
Misko Hevery <misko@hevery.com>
2021-08-09 16:31:50 -0700

    chore: adda `.bazelversion` so that repo can be used off-Lines
    ALso fixed typos

--- packages/core/src/helpers/parse-node.ts
+++ packages/core/src/helpers/parse-node.ts
@@ -2,14 +2,14 @@ import { parseJsx } from '../parsers/jsx';
 
 export const parseNode = (str: string) => {
   return parseJsx(`
-    export default function MyCompoennt() {
+    export default function MyComponent() {
       return (${str})
     }
   `).children[0];
 };
 export const parseNodes = (str: string) => {
   return parseJsx(`
-    export default function MyCompoennt() {
+    export default function MyComponent() {
       return (<>${str}</>)
     }
   `).children[0].children;
