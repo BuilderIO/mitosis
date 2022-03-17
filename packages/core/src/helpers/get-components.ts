61c7d482fa3a08258a7c80d4b9dc0b0d1e7163a3
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2022-03-18 00:37:47 +0300

    prettier

--- packages/core/src/helpers/get-components.ts
+++ packages/core/src/helpers/get-components.ts
@@ -5,7 +5,7 @@ import { isUpperCase } from './is-upper-case';
 
 export function getComponents(json: MitosisComponent): Set<string> {
   const components = new Set<string>();
-  traverse(json).forEach(function(item) {
+  traverse(json).forEach(function (item) {
     if (isMitosisNode(item)) {
       if (isUpperCase(item.name[0])) {
         components.add(item.name);
--- packages/core/src/helpers/has-component.ts
+++ packages/core/src/helpers/has-component.ts
@@ -4,7 +4,7 @@ import { isMitosisNode } from './is-mitosis-node';
 
 export const hasComponent = (name: string, json: MitosisComponent) => {
   let has = false;
-  traverse(json).forEach(function(item) {
+  traverse(json).forEach(function (item) {
     if (isMitosisNode(item)) {
       if (item.name === name) {
         has = true;
