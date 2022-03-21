f1815b6a8aeb8b5e1c826a60ed1aab26ffbca38f
Sachin Raja <sachinraja2349@gmail.com>
2022-03-21 16:56:32 -0700

    remove unneeded dependencies

--- packages/core/src/internal-types.d.ts
+++ packages/core/src/internal-types.d.ts
@@ -1,6 +1,1 @@
 declare module '@fake*';
-
-declare module 'rollup/dist/rollup.browser.es' {
-  const exported: typeof import('rollup');
-  export = exported;
-}
--- packages/core/typings.d.ts
+++ packages/core/typings.d.ts
@@ -1,6 +1,2 @@
-declare module 'json-6' {
-  export = JSON;
-}
-
 // Dummy module for tests
 declare module '@dummy*';
