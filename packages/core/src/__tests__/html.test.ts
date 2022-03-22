242dca3cbcba3f4e420efbc2f14be596e0a4e761
Sami Jaber <jabersami@gmail.com>
2022-03-22 11:30:55 -0400

    tests(html): add shadow DOM test, update snapshots

--- packages/core/src/__tests__/html.test.ts
+++ packages/core/src/__tests__/html.test.ts
@@ -2,10 +2,16 @@ import { componentToHtml } from '../generators/html';
 import { parseJsx } from '../parsers/jsx';
 
 const stamped = require('./data/blocks/stamped-io.raw');
+const shadowDom = require('./data/blocks/shadow-dom.raw');
 
 describe('Html', () => {
   test('Stamped', () => {
     const component = parseJsx(stamped);
+    const html = componentToHtml()({ component });
+    expect(html).toMatchSnapshot();
+  });
+  test('Shadow DOM', () => {
+    const component = parseJsx(shadowDom);
     const html = componentToHtml()({ component });
     expect(html).toMatchSnapshot();
   });
