8c7e5ccdbb5fdbfd98901fd855a9f822cab98eaa
Sami Jaber <jabersami@gmail.com>
2022-03-28 14:23:17 -0400

    fix(react): process bindings in onUpdate deps (#245)
    * fix(react): process bindings in onUpdate deps
    
    * chore: spacing

--- packages/core/src/generators/react.ts
+++ packages/core/src/generators/react.ts
@@ -588,7 +588,15 @@ const _componentToReact = (
               updateStateSettersInCode(json.hooks.onUpdate.code, options),
               options,
             )}
-          }, ${json.hooks.onUpdate.deps})`
+          }, 
+          ${
+            json.hooks.onUpdate.deps
+              ? processBinding(
+                  updateStateSettersInCode(json.hooks.onUpdate.deps, options),
+                  options,
+                )
+              : ''
+          })`
           : ''
       }
 
