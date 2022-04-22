0f6ffb4f51ea7696e7b2b8c28be6aec11e09743e
Sanyam Kamat <sanyamkamat@gmail.com>
2022-04-22 10:12:47 +0530

    fix(svelte): support self-referencing component with children (#291)

--- packages/core/src/__tests__/svelte.test.ts
+++ packages/core/src/__tests__/svelte.test.ts
@@ -4,6 +4,7 @@ import { parseJsx } from '../parsers/jsx';
 const onUpdate = require('./data/blocks/onUpdate.raw');
 const multipleOUpdate = require('./data/blocks/multiple-onUpdate.raw');
 const selfReferencingComponent = require('./data/blocks/self-referencing-component.raw');
+const selfReferencingComponentWithChildren = require('./data/blocks/self-referencing-component-with-children.raw');
 
 describe('Svelte', () => {
   test('onUpdate', () => {
@@ -20,6 +21,12 @@ describe('Svelte', () => {
 
   test('selfReferencingComponent', () => {
     const component = parseJsx(selfReferencingComponent);
+    const output = componentToSvelte()({ component });
+    expect(output).toMatchSnapshot();
+  });
+
+  test('selfReferencingComponentWithChildren', () => {
+    const component = parseJsx(selfReferencingComponentWithChildren);
     const output = componentToSvelte()({ component });
     expect(output).toMatchSnapshot();
   });
