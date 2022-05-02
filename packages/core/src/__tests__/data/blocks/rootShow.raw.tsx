418406badf49a57f9f3ff8c56e3397072ae90a81
Sami Jaber <jabersami@gmail.com>
2022-05-02 15:26:43 -0400

    fix types

--- packages/core/src/__tests__/data/blocks/rootShow.raw.tsx
+++ packages/core/src/__tests__/data/blocks/rootShow.raw.tsx
@@ -1,6 +1,6 @@
 import { Show } from '@builder.io/mitosis';
 
-export default function RenderStyles(props) {
+export default function RenderStyles(props: { foo: string }) {
   return (
     <Show when={props.foo === 'bar'} else={<div>Foo</div>}>
       <div>Bar</div>
