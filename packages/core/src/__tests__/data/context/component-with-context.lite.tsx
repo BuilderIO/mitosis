a0a108b2f8416625b7841ad065b6d8fd3f4d9f23
Sami Jaber <jabersami@gmail.com>
2022-05-03 16:54:55 -0400

    add tests

--- packages/core/src/__tests__/data/context/component-with-context.lite.tsx
+++ packages/core/src/__tests__/data/context/component-with-context.lite.tsx
@@ -2,10 +2,15 @@ import Context1 from '@dummy/1';
 import Context2 from '@dummy/2';
 import { useContext, setContext } from '@builder.io/mitosis';
 
-export default function ComponentWithContext() {
+export default function ComponentWithContext(props: { content: string }) {
   const foo = useContext(Context1);
 
-  setContext(Context1, { foo: 'bar' });
+  setContext(Context1, {
+    foo: 'bar',
+    content() {
+      return props.content;
+    },
+  });
 
   return (
     <Context2.Provider value={{ bar: 'baz' }}>
