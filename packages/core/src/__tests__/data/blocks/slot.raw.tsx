d40fdf72f6fb6ba05cc087ffa89142ea7abd8ace
Andrew Lu <andrew.lu@experian.com>
2022-05-10 20:45:40 -0400

    added snaps

--- packages/core/src/__tests__/data/blocks/slot.raw.tsx
+++ packages/core/src/__tests__/data/blocks/slot.raw.tsx
@@ -1,5 +1,13 @@
 type Props = { [key: string]: string };
 
 export default function SlotCode(props: Props) {
-  return <div>{props.children}</div>;
+  return (
+    <div>
+      <Slot name={props.testing} />
+      <div>
+        <OtherComponentWithSlot slotRight={<someContent></someContent>} />
+      </div>
+      <div>{props.children}</div>
+    </div>
+  );
 }
