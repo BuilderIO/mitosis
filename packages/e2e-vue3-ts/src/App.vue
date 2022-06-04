2e41cbbc696ffe13b3c8c2352f14978adb1c409d
Kyle Cordes <kyle.cordes@oasisdigital.com>
2022-06-04 09:10:58 -0500

    Test harness for Vue 3 output
    Builds and runs

---
+++ packages/e2e-vue3-ts/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import MyComponent from '@builder.io/e2e-app/vue3/components/my-component.vue';
+</script>
+
+<template>
+  <MyComponent></MyComponent>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
