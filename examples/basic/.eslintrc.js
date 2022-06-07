feee99821d4375f8360425d27f1dc05d9f3460fb
Sami Jaber <jabersami@gmail.com>
2022-06-07 11:40:27 -0400

    examples: add eslint plugin to example

---
+++ examples/basic/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@builder.io/mitosis/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', '@builder.io/mitosis'],
+};
