c4e14fb3d2db1caae5903e9d3b72cadedfaf5981
decade <decadef20@gmail.com>
2022-06-13 22:37:23 +0800

    fix: swap props test filename

--- packages/core/src/__tests__/data/basic-props.raw.tsx
+++ packages/core/src/__tests__/data/basic-props.raw.tsx
@@ -1,14 +1,14 @@
 import { useState } from '@builder.io/mitosis';
 
 // @ts-ignore
-export default function MyBasicComponent({ children: c, type }) {
+export default function MyBasicComponent(props) {
   const state = useState({
     name: 'Decadef20',
   });
 
   return (
     <div>
-      {c} {type}
+      {props.children} {props.type}
       Hello! I can run in React, Vue, Solid, or Liquid!
     </div>
   );
