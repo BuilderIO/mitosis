00a2348e746ed79a8c6e9a39a0911cc181886cc9
decadef20 <decadef20@gmail.com>
2022-06-15 17:04:43 +0800

    fix: Component exports not including Functions

---
+++ packages/core/src/__tests__/data/basic-preserve-export-or-local-statement.raw.tsx
@@ -0,0 +1,18 @@
+const b = 3;
+const foo = () => {};
+
+export const a = 3;
+export const bar = () => {};
+export function run<T>(value: T) {}
+
+type Types = {
+  s: any[];
+};
+
+interface IPost {
+  len: number;
+}
+
+export default function MyBasicComponent(props: { id: string }) {
+  return <div></div>;
+}
--- packages/core/src/parsers/jsx.ts
+++ packages/core/src/parsers/jsx.ts
@@ -982,31 +982,50 @@ export function parseJsx(
             context.builder.component.exports = exportsOrLocalVariables.reduce(
               (pre, node) => {
                 let name, isFunction;
-                if (
-                  babel.types.isExportNamedDeclaration(node) &&
-                  babel.types.isVariableDeclaration(node.declaration) &&
-                  babel.types.isIdentifier(node.declaration.declarations[0].id)
-                ) {
-                  name = node.declaration.declarations[0].id.name;
-                  isFunction = babel.types.isFunction(
-                    node.declaration.declarations[0].init,
-                  );
-                } else if (
-                  babel.types.isVariableDeclaration(node) &&
-                  babel.types.isIdentifier(node.declarations[0].id)
-                ) {
-                  name = node.declarations[0].id.name;
-                  isFunction = babel.types.isFunction(
-                    node.declarations[0].init,
-                  );
+                if (babel.types.isExportNamedDeclaration(node)) {
+                  if (
+                    babel.types.isVariableDeclaration(node.declaration) &&
+                    babel.types.isIdentifier(
+                      node.declaration.declarations[0].id,
+                    )
+                  ) {
+                    name = node.declaration.declarations[0].id.name;
+                    isFunction = babel.types.isFunction(
+                      node.declaration.declarations[0].init,
+                    );
+                  }
+
+                  if (babel.types.isFunctionDeclaration(node.declaration)) {
+                    name = node.declaration.id?.name;
+                    isFunction = true;
+                  }
+                } else {
+                  if (
+                    babel.types.isVariableDeclaration(node) &&
+                    babel.types.isIdentifier(node.declarations[0].id)
+                  ) {
+                    name = node.declarations[0].id.name;
+                    isFunction = babel.types.isFunction(
+                      node.declarations[0].init,
+                    );
+                  }
+
+                  if (babel.types.isFunctionDeclaration(node)) {
+                    name = node.id?.name;
+                    isFunction = true;
+                  }
                 }
+
                 if (name) {
                   pre[name] = {
                     code: generate(node).code,
                     isFunction,
                   };
                 } else {
-                  console.warn('export statement without name', node);
+                  console.warn(
+                    'Could not parse export or variable: ignoring node',
+                    node,
+                  );
                 }
                 return pre;
               },
