faa5910c05cc554b22a69a55e18440ecc84dcf0e
Sami Jaber <jabersami@gmail.com>
2022-06-15 15:24:08 -0400

    chore(core): delete integrations (#489)

--- packages/core/integration/frameworks/solid/static/writeToDisk.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const server = require(process.argv[2]);
-
-async function write() {
-  const res = await server({ url: process.argv[4] });
-  fs.writeFile(process.argv[3], res, () => process.exit(0));
-}
-write();
