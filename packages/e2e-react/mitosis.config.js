b569fbf1e9cdd1166822fe00c913dc98a10ae763
Kyle Cordes <kyle.cordes@oasisdigital.com>
2022-06-26 10:46:18 -0500

    React E2E: consume Mitosis output as package

--- packages/e2e-react/mitosis.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  files: 'src/**',
-  dest: 'react_src/lib',
-  targets: ['react'],
-  options: {
-    react: { transpiler: { format: 'esm' } },
-  },
-};
---
+++ packages/e2e-react/src/App.tsx
@@ -0,0 +1,9 @@
+// Mitosis output for not yet include .d.ts, so ignore the types when importing.
+// @ts-ignore
+import MyComponent from '@builder.io/e2e-app/react/components/my-component.lite';
+
+function App() {
+  return <MyComponent></MyComponent>;
+}
+
+export default App;
