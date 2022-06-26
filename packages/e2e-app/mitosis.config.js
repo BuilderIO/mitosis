66480600ce2af55655d20b6cd0a6b79e9ed629c1
Kyle Cordes <kyle.cordes@oasisdigital.com>
2022-06-26 10:32:33 -0500

    E2E app: avoid output path conflict between concurrent runs

--- packages/e2e-app/mitosis.config.js
+++ packages/e2e-app/mitosis.config.js
@@ -4,6 +4,8 @@ module.exports = {
   // for this E2E test, we vary it by invocation to separately test each target,
   // so that one failing does not stop the whole test suite.
   targets: [process.argv[3]],
+  // Each run needs a separate output dest, as Mitosis clears it first.
+  dest: ['output/' + process.argv[3]],
   options: {
     react: { transpiler: { format: 'esm' } },
   },
