bc8c6b8bcce6d24ead827a92064a0d5c517f78d9
Sami Jaber <jabersami@gmail.com>
2022-06-29 22:17:24 -0400

    fix import

--- packages/core/src/helpers/styles/collect-css.test.ts
+++ packages/core/src/helpers/styles/collect-css.test.ts
@@ -2,7 +2,7 @@ import { collectCss } from './collect-css';
 import { parseJsx } from '../../parsers/jsx';
 
 const classRaw = require('../../__tests__/data/styles/class.raw');
-const classState = require('../../__tests__/data/styles/classstate.raw');
+const classState = require('../../__tests__/data/styles/classState.raw');
 
 describe('Styles', () => {
   test('class property and CSS are merged', () => {
