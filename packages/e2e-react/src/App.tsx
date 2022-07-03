1c6235ba04be96bb442dd6271e2c16ea61b7bed2
Kyle Cordes <kyle.cordes@oasisdigital.com>
2022-07-02 19:44:30 -0500

    E2E React and Solid - more concise import, newer versions

--- packages/e2e-react/src/App.tsx
+++ packages/e2e-react/src/App.tsx
@@ -1,6 +1,10 @@
 // Mitosis output for not yet include .d.ts, so ignore the types when importing.
 // @ts-ignore
-import MyComponent from '@builder.io/e2e-app/react/components/my-component.lite';
+import { MyComponent } from '@builder.io/e2e-app/react';
+import React from 'react';
+
+// Accommodate Mitosis output - assumes React is a global.
+window.React = React;
 
 function App() {
   return <MyComponent></MyComponent>;
