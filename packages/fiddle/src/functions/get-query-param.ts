7a8b92a37036f67ea883190ecce89632f490f3bf
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2022-07-20 22:26:06 +0300

    feat: add string compression to query params getter and setter

--- packages/fiddle/src/functions/get-query-param.ts
+++ packages/fiddle/src/functions/get-query-param.ts
@@ -1,4 +1,8 @@
+import { decompressFromBase64 } from 'lz-string';
+
 export const getQueryParam = (name: string): string | null => {
   const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get(name);
+  const compressedValue = urlParams.get(name);
+
+  return decompressFromBase64(compressedValue);
 };
