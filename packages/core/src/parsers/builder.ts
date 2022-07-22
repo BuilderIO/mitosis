cb525eb492edecd5f8b6802959b09b9bb001ab14
Misko Hevery <misko@hevery.com>
2022-07-22 15:20:10 -0700

    fix(qwik): update to the latest Qwik format

--- packages/core/src/parsers/builder.ts
+++ packages/core/src/parsers/builder.ts
@@ -308,20 +308,21 @@ const componentMappers: {
     delete node.bindings.columns;
     delete node.properties.columns;
 
-    node.children = block.component?.options.columns?.map((col: any, index: number) =>
-      createMitosisNode({
-        name: 'Column',
-        bindings: {
-          width: { code: col.width },
-        },
-        ...(col.link && {
-          properties: {
-            link: col.link,
+    node.children =
+      block.component?.options.columns?.map((col: any, index: number) =>
+        createMitosisNode({
+          name: 'Column',
+          bindings: {
+            width: { code: col.width },
           },
+          ...(col.link && {
+            properties: {
+              link: col.link,
+            },
+          }),
+          children: col.blocks.map((col: any) => builderElementToMitosisNode(col, options)),
         }),
-        children: col.blocks.map((col: any) => builderElementToMitosisNode(col, options)),
-      }),
-    );
+      ) || [];
 
     return node;
   },
