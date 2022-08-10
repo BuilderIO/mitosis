bd184a053f6e2cc8dac0a7d628f8e6754f1776f6
Sami Jaber <jabersami@gmail.com>
2022-08-10 09:00:20 -0400

    Fix: Qwik/CLI JSON cloning (#622)
    * clone component in qwik generator
    
    * add fastClone to CLI
    
    * add fastClone

--- packages/cli/src/build/build.ts
+++ packages/cli/src/build/build.ts
@@ -21,6 +21,7 @@ import glob from 'fast-glob';
 import { outputFile, pathExists, readFile, remove } from 'fs-extra';
 import { kebabCase } from 'lodash';
 import micromatch from 'micromatch';
+import { fastClone } from '../helpers/fast-clone';
 import { buildContextFile } from './helpers/context';
 import { getFileExtensionForTarget } from './helpers/extensions';
 import { transpile } from './helpers/transpile';
@@ -109,10 +110,15 @@ export async function build(config?: MitosisConfig) {
 
   await Promise.all(
     targetContexts.map(async (targetContext) => {
+      // clone mitosis JSONs for each target, so we can modify them in each generator without affecting future runs.
+      // each generator also clones the JSON before manipulating it, but this is an extra safety measure.
+      const files = fastClone(mitosisComponents);
+
       const targetContextWithConfig: TargetContextWithConfig = { ...targetContext, options };
+
       await Promise.all([
         buildAndOutputNonComponentFiles(targetContextWithConfig),
-        buildAndOutputComponentFiles({ ...targetContextWithConfig, files: mitosisComponents }),
+        buildAndOutputComponentFiles({ ...targetContextWithConfig, files }),
       ]);
       await outputOverrides(targetContextWithConfig);
     }),
---
+++ packages/cli/src/helpers/fast-clone.ts
@@ -0,0 +1,1 @@
+export const fastClone = <T extends object>(obj: T): T => JSON.parse(JSON.stringify(obj));
