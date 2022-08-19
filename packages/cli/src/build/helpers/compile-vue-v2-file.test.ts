9202dba4a1e65bb4800a522e14abdd57379e11f9
Sami Jaber <jabersami@gmail.com>
2022-08-18 20:58:38 -0400

    remove unused code

--- packages/cli/src/build/helpers/compile-vue-v2-file.test.ts
+++
@@ -1,8 +0,0 @@
-import { transpileBindingExpression } from './transpile-vue-binding-expression';
-
-test('Transforms binding expression optional member access', async () => {
-  expect(await transpileBindingExpression('foo?.bar?.baz')).toEqual(
-    // A little redundant, but effective for our needs
-    '((foo && foo.bar) && (foo && foo.bar).baz)',
-  );
-});
