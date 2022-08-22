85b096eaa7c0bbd48185aec32cf412896ab738af
Sami Jaber <jabersami@gmail.com>
2022-08-22 11:17:10 -0400

    undo merge change

--- packages/core/src/helpers/strip-state-and-props-refs.ts
+++ packages/core/src/helpers/strip-state-and-props-refs.ts
@@ -58,7 +58,7 @@ export const stripStateAndPropsRefs = (
 
   outputVars.forEach((_var) => {
     // determine expression edge cases onMessage( to this.onMessage.emit(
-    const regexp = '( |;|\\()(props\\.?)' + _var + '\\(';
+    const regexp = '(^|\\s|;|\\()(props\\.?)' + _var + '\\(';
     const replacer = '$1' + context + _var + '.emit(';
     newCode = newCode.replace(new RegExp(regexp, 'g'), replacer);
   });
