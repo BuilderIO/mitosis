218af6dc6a61fe8a14da36016462e396d4763fe3
Sami Jaber <jabersami@gmail.com>
2022-08-24 16:53:46 -0400

    add missing import

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -62,6 +62,7 @@ export * from './generators/template';
 export * from './generators/swift-ui';
 export * from './generators/lit';
 export * from './generators/react-native';
+export * from './helpers/parse-reactive-script';
 export * from './helpers/is-mitosis-node';
 
 export * from './types/mitosis-node';
