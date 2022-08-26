f25d92ccbe10bddd429b6d8d4a38f1c250469a40
Andrzej Kurek <32792205+Razz21@users.noreply.github.com>
2022-08-26 21:12:27 +0200

    feat(vue): convert prop slots to Vue template slot syntax (#666)
    * feat: prop slots to vue template slot syntax
    
    * refactor: babel transform slots
    
    * update test
    
    Co-authored-by: Sami Jaber <jabersami@gmail.com>

--- packages/core/src/helpers/slots.ts
+++ packages/core/src/helpers/slots.ts
@@ -1,5 +1,21 @@
+import { types } from '@babel/core';
+import { babelTransformExpression } from './babel-transform';
+
 const SLOT_PREFIX = 'slot';
+export type SlotMapper = (slotName: string) => string;
 
 export const isSlotProperty = (key: string): boolean => key.startsWith(SLOT_PREFIX);
 
 export const stripSlotPrefix = (key: string): string => key.substring(SLOT_PREFIX.length);
+
+export function replaceSlotsInString(code: string, mapper: SlotMapper) {
+  return babelTransformExpression(code, {
+    Identifier(path: babel.NodePath<babel.types.Identifier>) {
+      const name = path.node.name;
+      const isSlot = isSlotProperty(name);
+      if (isSlot) {
+        path.replaceWith(types.identifier(mapper(stripSlotPrefix(name).toLowerCase())));
+      }
+    },
+  });
+}
