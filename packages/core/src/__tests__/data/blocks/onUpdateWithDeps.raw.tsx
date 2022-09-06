13b9f7ad72d04f8f725a87ed8e1cb970d9e685db
Raymond Muller <raymond.muller.1989@gmail.com>
2022-09-06 16:54:38 +0200

    test: change test to show that props.* is handled correctly, by adding a props. entry to the onUpdate array

--- packages/core/src/__tests__/data/blocks/onUpdateWithDeps.raw.tsx
+++ packages/core/src/__tests__/data/blocks/onUpdateWithDeps.raw.tsx
@@ -1,14 +1,18 @@
 import { onUpdate, useStore } from '@builder.io/mitosis';
 
-export default function OnUpdateWithDeps() {
+type Props = {
+  size: string;
+};
+
+export default function OnUpdateWithDeps(props: Props) {
   const state = useStore({
     a: 'a',
     b: 'b',
   });
 
   onUpdate(() => {
-    console.log('Runs when a or b changes', state.a, state.b);
-  }, [state.a, state.b]);
+    console.log('Runs when a, b or size changes', state.a, state.b, props.size);
+  }, [state.a, state.b, props.size]);
 
   return <div />;
 }
