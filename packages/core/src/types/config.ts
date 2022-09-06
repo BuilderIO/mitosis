6abd9d45229c659bb306eb529b5d915df8927b10
Sami Jaber <jabersami@gmail.com>
2022-09-06 10:24:11 -0400

    Update packages/core/src/types/config.ts

--- packages/core/src/types/config.ts
+++ packages/core/src/types/config.ts
@@ -57,10 +57,10 @@ export type MitosisConfig = {
    * Configure the extension of the files you want to compile
    * Default value: 'lite.tsx'
    */
-  extension: string;
+  extension?: string;
   /**
    * Configure a custom parser function which takes a string and returns MitosisJSON
    * Defaults to the JSXParser of this project (src/parsers/jsx)
    */
-  parser: (code: string) => MitosisComponent;
+  parser?: (code: string) => MitosisComponent;
 };
