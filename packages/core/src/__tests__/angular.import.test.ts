044324f926c05afb58fe98c73c45336266d3686f
Andrew Lu <andrew.lu@experian.com>
2022-09-23 15:33:58 -0400

    Pruned excessive tests.

--- packages/core/src/__tests__/angular.import.test.ts
+++ packages/core/src/__tests__/angular.import.test.ts
@@ -2,7 +2,6 @@ import { componentToAngular } from '../generators/angular';
 import { runTestsForTarget } from './shared';
 
 describe('Angular with Preserve Imports', () => {
-  runTestsForTarget({ options: {}, target: 'angular', generator: componentToAngular });
   runTestsForTarget({
     options: {
       preserveImports: true,
