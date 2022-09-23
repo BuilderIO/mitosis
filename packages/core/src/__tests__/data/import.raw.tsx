5a02157f7b6e3315cabba2e7d401d3447d13ff4f
Andrew Lu <andrew.lu@experian.com>
2022-09-23 14:39:08 -0400

    Disabling lines for fake imports that lead to no modules. They are raw files not meant to be run.

--- packages/core/src/__tests__/data/import.raw.tsx
+++ packages/core/src/__tests__/data/import.raw.tsx
@@ -1,7 +1,7 @@
-import { default as Button1 } from './button';
-import Button2 from './button';
-import * as Button3 from './button';
-import { Button } from './button';
+import { default as Button1 } from './button'; // eslint-disable-line
+import Button2 from './button'; // eslint-disable-line
+import * as Button3 from './button'; // eslint-disable-line
+import { Button } from './button'; // eslint-disable-line
 
 export default function MyImportComponent() {
   return <div> Testing which imports get excluded! </div>;
