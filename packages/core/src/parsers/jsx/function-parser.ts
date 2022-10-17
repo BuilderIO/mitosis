e65d85db7190cb3fc7867c85cdace1ecae271233
Raymond Muller <raymondmuller@users.noreply.github.com>
2022-10-17 16:16:56 +0200

    fix(core): #819 use default props hook outside component body (#824)
    * fix(core): use default props hook outside component body
    
    * test(core): add test for default props outside component body

--- packages/core/src/parsers/jsx/function-parser.ts
+++ packages/core/src/parsers/jsx/function-parser.ts
@@ -19,6 +19,26 @@ export function generateUseStyleCode(expression: babel.types.CallExpression) {
   return generate(expression.arguments[0]).code.replace(/(^("|'|`)|("|'|`)$)/g, '');
 }
 
+export function parseDefaultPropsHook(
+  component: MitosisComponent,
+  expression: babel.types.CallExpression,
+) {
+  const firstArg = expression.arguments[0];
+
+  if (types.isObjectExpression(firstArg)) {
+    const objectProperties = firstArg.properties?.filter((i) => types.isObjectProperty(i));
+    objectProperties?.forEach((i: any) => {
+      console.log({ i });
+      if (i.key?.name) {
+        component.defaultProps = {
+          ...(component.defaultProps ?? {}),
+          [i.key?.name]: i.value.value,
+        };
+      }
+    });
+  }
+}
+
 /**
  * Parses function declarations within the Mitosis copmonent's body to JSON
  */
@@ -135,20 +155,7 @@ export const componentFunctionToJson = (
               hooks.onInit = { code };
             }
           } else if (expression.callee.name === HOOKS.DEFAULT_PROPS) {
-            const firstArg = expression.arguments[0];
-            if (types.isObjectExpression(firstArg)) {
-              const objectProperties = firstArg.properties?.filter((i) =>
-                types.isObjectProperty(i),
-              );
-              objectProperties?.forEach((i: any) => {
-                if (i.key?.name) {
-                  context.builder.component.defaultProps = {
-                    ...(context.builder.component.defaultProps ?? {}),
-                    [i.key?.name]: i.value.value,
-                  };
-                }
-              });
-            }
+            parseDefaultPropsHook(context.builder.component, expression);
           } else if (expression.callee.name === HOOKS.STYLE) {
             context.builder.component.style = generateUseStyleCode(expression);
           }
