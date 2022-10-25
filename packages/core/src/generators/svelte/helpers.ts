4fbf6e11d8665e1f40d5e3e6d238bad27f371868
Sami Jaber <jabersami@gmail.com>
2022-10-25 19:56:41 -0400

    generate reactive svelte context

--- packages/core/src/generators/svelte/helpers.ts
+++ packages/core/src/generators/svelte/helpers.ts
@@ -1,21 +1,6 @@
 import { MitosisComponent } from '../../types/mitosis-component';
 import { stripStateAndPropsRefs } from '../../helpers/strip-state-and-props-refs';
 import { ToSvelteOptions } from './types';
-import { replaceIdentifiers } from '../../helpers/replace-identifiers';
-
-function getContextNames(json: MitosisComponent) {
-  return Object.keys(json.context.get);
-}
-
-export const replaceContextGetters =
-  ({ json }: { json: MitosisComponent }) =>
-  (code: string) => {
-    return replaceIdentifiers({
-      code,
-      from: getContextNames(json),
-      to: (name) => `$${name}`,
-    });
-  };
 
 export const stripStateAndProps =
   ({ options, json }: { options: ToSvelteOptions; json: MitosisComponent }) =>
