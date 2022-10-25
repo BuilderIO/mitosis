1b5531f905a80aca50b2a853a69ab4578fa344b3
Sami Jaber <jabersami@gmail.com>
2022-10-25 16:13:55 -0400

    make svelte context reactive

--- packages/core/src/generators/svelte/helpers.ts
+++ packages/core/src/generators/svelte/helpers.ts
@@ -1,8 +1,26 @@
+import { MitosisComponent } from '../../types/mitosis-component';
 import { stripStateAndPropsRefs } from '../../helpers/strip-state-and-props-refs';
 import { ToSvelteOptions } from './types';
+import { replaceIdentifiers } from '../../helpers/replace-identifiers';
 
-export const stripStateAndProps = (code: string | undefined, options: ToSvelteOptions) =>
-  stripStateAndPropsRefs(code, {
-    includeState: options.stateType === 'variables',
-    replaceWith: (name) => (name === 'children' ? '$$slots.default' : name),
-  });
+function getContextNames(json: MitosisComponent) {
+  return Object.keys(json.context.get);
+}
+
+export const replaceContextGetters =
+  ({ json }: { json: MitosisComponent }) =>
+  (code: string) => {
+    return replaceIdentifiers({
+      code,
+      from: getContextNames(json),
+      to: (name) => `$${name}`,
+    });
+  };
+
+export const stripStateAndProps =
+  ({ options, json }: { options: ToSvelteOptions; json: MitosisComponent }) =>
+  (code: string) =>
+    stripStateAndPropsRefs(code, {
+      includeState: options.stateType === 'variables',
+      replaceWith: (name) => (name === 'children' ? '$$slots.default' : name),
+    });
