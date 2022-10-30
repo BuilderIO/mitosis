5847a71f065179998b4085e34cff0728c6b14d5a
kingzez <Vincent.Mr.W@gmail.com>
2022-10-31 01:26:07 +0800

    fix(cli): transformImports better RegExp for replace extension

--- packages/cli/src/build/helpers/transpile.ts
+++ packages/cli/src/build/helpers/transpile.ts
@@ -13,12 +13,12 @@ export const transformImports = (target: Target, options: MitosisConfig) => (cod
       // we start by replacing all `context.lite` imports with `context`
       // This Context replace is only needed for non-mitosis components, i.e. plain `.js`/`.ts` files.
       // Mitosis components have logic that transform context import paths correctly.
-      /\.context\.lite(['"][;\)])/g,
+      /\.context\.lite/g,
       `.context.js$1`,
     )
     .replace(
       // afterwards, we replace all `.lite` imports with the correct file extension
-      /\.lite(['"][;\)])/g,
+      /\.lite/g,
       `${getFileExtensionForTarget({ type: 'import', target, options })}$1`,
     )
     .replace(
