60f93a4499f0aa16627db5662bd4adddc1fae9db
Spencer Brower <brower.spencer@gmail.com>
2022-10-29 21:53:07 -0400

    feat: Basic `$watch` hooks now get rendered.

--- packages/core/src/generators/alpine/render-mount-hook.ts
+++ packages/core/src/generators/alpine/render-mount-hook.ts
@@ -1,8 +1,17 @@
 import { curry } from 'lodash';
 import { MitosisComponent } from '../../types/mitosis-component';
+import { hasRootUpdateHook, renderWatchHooks } from './render-update-hooks';
+
+function shouldRenderMountHook(json: MitosisComponent): boolean {
+  return json.hooks.onMount !== undefined
+      || hasRootUpdateHook(json);
+}
 
 export const renderMountHook = curry((json: MitosisComponent, objectString: string) => {
-  return json.hooks.onMount
-    ? objectString.replace(/(?:,)?(\s*)(}\s*)$/, `, init() {${json.hooks.onMount?.code}}$1$2`)
+  return shouldRenderMountHook(json)
+    ? objectString.replace(/(?:,)?(\s*)(}\s*)$/, `, init() {
+      ${renderWatchHooks(json)}
+      ${json.hooks.onMount?.code ?? ''}
+    }$1$2`)
     : objectString;
 });
--- packages/core/src/generators/alpine/render-update-hooks.ts
+++ packages/core/src/generators/alpine/render-update-hooks.ts
@@ -24,3 +24,25 @@ export function hasRootUpdateHook(json: MitosisComponent): boolean {
 export const renderUpdateHooks = curry((json: MitosisComponent, output: string) => {
     return renderRootUpdateHook(getRootUpdateHooks(json), output);
 });
+
+function getWatchHooks(json: MitosisComponent) {
+    return (json.hooks.onUpdate ?? []).filter(hook => hook.deps?.match('state')) 
+}
+
+const hasWatchHooks = (json: MitosisComponent): boolean => {
+    return getWatchHooks(json).length > 0
+}
+
+function renderWatchHook(hook: extendedHook): string {
+    const deps = (hook.deps ?? '')?.slice(1).slice(0, -1).split(', ')
+    .filter(dep => dep.match(/state|this/));
+
+
+    return deps.map(dep => `this.$watch('${dep.replace(/(state|this)\./, '')}', (value, oldValue) => { ${hook.code} });`).join('\n')
+}
+
+export const renderWatchHooks = (json: MitosisComponent): string => {
+    return hasWatchHooks(json)
+    ? getWatchHooks(json).map(renderWatchHook).join('\n')
+    : ''
+}
