71da66cf2a13d235a572721288931f4cdce8b144
PatrickJS <github@gdi2290.com>
2022-11-02 08:57:24 -0700

    Update packages/cli/src/build/helpers/transpile.ts
    Co-authored-by: Sami Jaber <jabersami@gmail.com>

--- packages/cli/src/build/helpers/transpile.ts
+++ packages/cli/src/build/helpers/transpile.ts
@@ -18,7 +18,7 @@ export const transformImports = (target: Target, options: MitosisConfig) => (cod
     )
     .replace(
       // afterwards, we replace all `.lite` imports with the correct file extension
-      /\.lite/g,
+      /\.lite['"]/g,
       `${getFileExtensionForTarget({ type: 'import', target, options })}$1`,
     )
     .replace(
