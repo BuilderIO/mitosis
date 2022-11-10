4e9a1617711f96bf05ccd6d2427610128eb5f0cd
Spencer Brower <brower.spencer@gmail.com>
2022-11-09 20:01:18 -0500

    fix: Updated Alpine.js to output an object.

--- packages/core/src/generators/alpine/generate.ts
+++ packages/core/src/generators/alpine/generate.ts
@@ -241,5 +241,5 @@ export const componentToAlpine: TranspilerGenerator<ToAlpineOptions> =
     if (options.plugins) {
       str = runPostCodePlugins(str, options.plugins);
     }
-    return str;
+    return [{ content: str, type: 'component' }];
   };
