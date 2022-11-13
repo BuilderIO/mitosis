c3551f24f0980d7c7ed792abe230d175ad63d6fe
Sami Jaber <jabersami@gmail.com>
2022-11-13 15:13:46 -0400

    fix bug

--- packages/core/src/generators/helpers/context.ts
+++ packages/core/src/generators/helpers/context.ts
@@ -4,7 +4,7 @@ export const hasContext = (component: MitosisComponent) =>
   hasSetContext(component) || hasGetContext(component);
 
 export const hasSetContext = (component: MitosisComponent) =>
-  Object.keys(component.context.get).length > 0;
+  Object.keys(component.context.set).length > 0;
 
 export const hasGetContext = (component: MitosisComponent) =>
-  Object.keys(component.context.set).length > 0;
+  Object.keys(component.context.get).length > 0;
