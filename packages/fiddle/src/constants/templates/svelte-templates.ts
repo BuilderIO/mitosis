44075d75ce72589ce1614ddf4659ee73d31db4f6
Raymond Muller <raymond.muller.1989@gmail.com>
2022-11-15 11:59:16 +0100

    feat(fiddle):  add another svelte template

--- packages/fiddle/src/constants/templates/svelte-templates.ts
+++ packages/fiddle/src/constants/templates/svelte-templates.ts
@@ -40,6 +40,25 @@ export const templates: { [key: string]: string } = {
     </div>
 
   `,
+  'reactive 2': dedent`
+    <script>
+      let a = 2
+      let b = 5
+      let result;
+
+      $: calculateResult(a, b)
+
+      function calculateResult(a, b) {
+        result = a * b
+      }
+    </script>
+
+    <div>
+      <input type="number" bind:value={a} />
+      <input type="number" bind:value={b} />
+      Result: {result}
+    </div>
+  `,
 
   'if / else': dedent`
     <script>
