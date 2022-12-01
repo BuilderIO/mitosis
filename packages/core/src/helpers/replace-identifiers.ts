2cda94b51d0e00ca8d0394e768dccd5f282f4dc9
Sami Jaber <jabersami@gmail.com>
2022-12-01 10:20:43 +0100

    fix infinite loop in replaceIdentifiers

--- packages/core/src/helpers/replace-identifiers.ts
+++ packages/core/src/helpers/replace-identifiers.ts
@@ -81,6 +81,21 @@ const _replaceIdentifiers = (
               }
             },
           );
+
+          /**
+           * If both `path` and `newMemberExpression` are identifier nodes that are equal, do nothing.
+           * This is to prevent infinite loops when the user-provided `to` function returns the same identifier.
+           *
+           * The infinite loop probably happens because we end up traversing the new `Identifier` node again?
+           */
+          if (
+            types.isIdentifier(path.node) &&
+            types.isIdentifier(newMemberExpression) &&
+            path.node.name === newMemberExpression.name
+          ) {
+            return;
+          }
+
           path.replaceWith(newMemberExpression);
         } catch (err) {
           console.error('Could not replace', path.node, 'with', to);
