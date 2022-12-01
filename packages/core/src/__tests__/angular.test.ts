9be3864013d8ade87ddeddf2bf83bfc9c74d6689
Ruben Piatnitsky <ruben.piatnitsky@experian.com>
2022-11-30 17:09:59 -0800

    Add test raw and snapshot changes

--- packages/core/src/__tests__/angular.test.ts
+++ packages/core/src/__tests__/angular.test.ts
@@ -1,8 +1,5 @@
 import { componentToAngular } from '../generators/angular';
 import { runTestsForTarget } from './shared';
-import { parseJsx } from "../parsers/jsx";
-
-import outputEventBindingExample from './data/output-event-bindings.raw.tsx?raw';
 
 describe('Angular', () => {
   runTestsForTarget({ options: {}, target: 'angular', generator: componentToAngular });
@@ -12,12 +9,5 @@ describe('Angular', () => {
     },
     target: 'angular',
     generator: componentToAngular,
-  });
-
-  test.only('Output event bindings properly formatted', () => {
-    const component = parseJsx(outputEventBindingExample);
-    expect(component).toMatchSnapshot();
-    const angularComponent = componentToAngular()({ component });
-    expect(angularComponent).toMatchSnapshot();
   });
 });
