57ebc0099cf43336894fbdb23da7487abb090006
Ruben Piatnitsky <ruben.piatnitsky@experian.com>
2022-12-08 08:26:41 -0800

    Run prettier

--- packages/core/src/__tests__/data/output-event-bindings.raw.tsx
+++ packages/core/src/__tests__/data/output-event-bindings.raw.tsx
@@ -1,16 +1,16 @@
-import { useState } from "@builder.io/mitosis";
+import { useState } from '@builder.io/mitosis';
 
 export default function MyComponent(props) {
-    const [name, setName] = useState("Steve");
+  const [name, setName] = useState('Steve');
 
-    return (
-        <div>
-            <input
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-                onChangeOrSomething={(event) => setName(event.target.value)}
-            />
-            Hello! I can run in React, Vue, Solid, or Liquid!
-        </div>
-    );
+  return (
+    <div>
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        onChangeOrSomething={(event) => setName(event.target.value)}
+      />
+      Hello! I can run in React, Vue, Solid, or Liquid!
+    </div>
+  );
 }
