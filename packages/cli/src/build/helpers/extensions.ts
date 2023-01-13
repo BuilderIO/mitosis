edd2c5bfa228c7438d7d169629e53717770a869d
Sami Jaber <jabersami@gmail.com>
2023-01-13 15:30:59 -0400

    fix react imports

--- packages/cli/src/build/helpers/extensions.ts
+++ packages/cli/src/build/helpers/extensions.ts
@@ -31,10 +31,17 @@ export const getFileExtensionForTarget = ({
     case 'lit':
       return '.ts';
     case 'qwik':
+      return isTs && type === 'filename' ? '.tsx' : '.jsx';
     case 'react':
     case 'reactNative':
     case 'rsc':
-      return isTs && type === 'filename' ? '.tsx' : '.jsx';
+      switch (type) {
+        // we can't have `.jsx`/`.tsx` extensions in the import paths.
+        case 'import':
+          return isTs ? '.ts' : '.js';
+        case 'filename':
+          return isTs ? '.tsx' : '.jsx';
+      }
     case 'marko':
       return '.marko';
     default:
