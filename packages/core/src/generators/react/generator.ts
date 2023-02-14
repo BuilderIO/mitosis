267deea8a71e32f6130640cbf0bbd24c7d176225
Sami Jaber <jabersami@gmail.com>
2023-02-14 08:38:48 -0400

    move

--- packages/core/src/generators/react/generator.ts
+++ packages/core/src/generators/react/generator.ts
@@ -380,21 +380,23 @@ const _componentToReact = (
         : ''
     }
     ${hasStateArgument ? '' : refsString}
-    ${
-      hasState
-        ? options.stateType === 'mobx'
-          ? `const state = useLocalObservable(() => (${getStateObjectStringFromComponent(json)}));`
-          : options.stateType === 'useState'
-          ? useStateCode
-          : options.stateType === 'solid'
-          ? `const state = useMutable(${getStateObjectStringFromComponent(json)});`
-          : options.stateType === 'builder'
-          ? `const state = useBuilderState(${getStateObjectStringFromComponent(json)});`
-          : options.stateType === 'variables'
-          ? `const state = ${getStateObjectStringFromComponent(json)};`
-          : `const state = useLocalProxy(${getStateObjectStringFromComponent(json)});`
-        : ''
-    }
+      ${
+        hasState
+          ? options.stateType === 'mobx'
+            ? `const state = useLocalObservable(() => (${getStateObjectStringFromComponent(
+                json,
+              )}));`
+            : options.stateType === 'useState'
+            ? useStateCode
+            : options.stateType === 'solid'
+            ? `const state = useMutable(${getStateObjectStringFromComponent(json)});`
+            : options.stateType === 'builder'
+            ? `const state = useBuilderState(${getStateObjectStringFromComponent(json)});`
+            : options.stateType === 'variables'
+            ? `const state = ${getStateObjectStringFromComponent(json)};`
+            : `const state = useLocalProxy(${getStateObjectStringFromComponent(json)});`
+          : ''
+      }
       ${hasStateArgument ? refsString : ''}
       ${getContextString(json, options)}
       ${getInitCode(json, options)}
