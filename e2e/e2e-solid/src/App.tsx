a4b2ab3c954edaf8a7976b2e84705875c70083f0
Sami Jaber <jabersami@gmail.com>
2023-03-08 17:41:21 -0400

    fdsa

--- e2e/e2e-solid/src/App.tsx
+++ e2e/e2e-solid/src/App.tsx
@@ -1,7 +1,5 @@
-// Mitosis output for not yet include .d.ts, so ignore the types when importing.
 // @ts-ignore
 import { E2eApp } from '@builder.io/e2e-app/solid';
-
 import type { Component } from 'solid-js';
 
 const App: Component = () => {
