e6a9e32dd42d8803cbe55f15f616890e1ec8a442
Sami Jaber <jabersami@gmail.com>
2023-03-10 12:40:36 -0400

    enable

--- e2e/e2e-app/playwright.config.ts
+++ e2e/e2e-app/playwright.config.ts
@@ -10,8 +10,8 @@ export const targets = [
   { packageName: 'e2e-react' },
   { packageName: 'e2e-solid' },
   { packageName: 'e2e-svelte' },
-  // { packageName: 'e2e-vue2' },
-  // { packageName: 'e2e-vue3' },
+  { packageName: 'e2e-vue2' },
+  { packageName: 'e2e-vue3' },
 ].map(({ packageName }, i) => {
   const port = 1234 + i;
 
