7cc18dd99b9f51793405d5e3b186eeca89571309
akshatgarg12 <akshatarungarg78@gmail.com>
2023-03-29 00:42:45 +0530

    added a flag for wrapElse

--- packages/core/src/generators/react/blocks.ts
+++ packages/core/src/generators/react/blocks.ts
@@ -106,12 +106,13 @@ const NODE_MAPPERS: {
   },
   Show(json, options, component) {
     const wrap = wrapInFragment(json);
+    const wrapElse = true; // TODO: make this configurable
     return `{${processBinding(json.bindings.when?.code as string, options)} ? (
       ${wrap ? openFrag(options) : ''}${json.children
       .filter(filterEmptyTextNodes)
       .map((item) => blockToReact(item, options, component))
       .join('\n')}${wrap ? closeFrag(options) : ''}
-    ) : ${!json.meta.else ? 'null' : (openFrag(options)) + blockToReact(json.meta.else as any, options, component) + (closeFrag(options))}}`;
+    ) : ${!json.meta.else ? 'null' : (wrapElse ? openFrag(options) : '') + blockToReact(json.meta.else as any, options, component) + (wrapElse ? closeFrag(options) : '')}}`;
   },
 };
 
