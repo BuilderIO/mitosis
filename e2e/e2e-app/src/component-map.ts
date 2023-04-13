6af5bd00c9056ee019a30d7062ffcb2c34cc3435
Akshat Garg <60404253+akshatgarg12@users.noreply.github.com>
2023-04-13 19:57:33 +0530

    Adding frag around else code in react Show directive (#1082)
    * null check in for directive of react
    
    * ran snapupdate
    
    * tracking yarn lock file
    
    * added show-for component in e2e app test
    
    * added a fragment around else code
    
    * remove flag for else wrap
    
    * added a flag for wrapElse
    
    * updated e2e tests for show-for component
    
    * snapshot updated
    
    * updated playwright tests and added checks for wrapElse
    
    * linting fix
    
    * updated tests
    
    * removed pnpm lockfile
    
    * lint fix
    
    * ran snapupdate
    
    ---------
    
    Co-authored-by: Sami Jaber <jabersami@gmail.com>

--- e2e/e2e-app/src/component-map.ts
+++ e2e/e2e-app/src/component-map.ts
@@ -1,9 +1,11 @@
 import oneComp from './one-component';
 import twoComp from './two-components';
 import typedComp from './types';
+import showForComp from './show-for-component';
 
 export const COMPONENT_MAP = {
   '/one-component/': oneComp,
   '/two-components/': twoComp,
   '/types/': typedComp,
+  '/show-for-component/': showForComp,
 };
