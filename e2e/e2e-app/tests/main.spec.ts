c2630f4c842f3275249b6c50e515e68c0ee880ab
Sami Jaber <jabersami@gmail.com>
2023-05-23 15:15:36 -0300

    k

--- e2e/e2e-app/tests/main.spec.ts
+++ e2e/e2e-app/tests/main.spec.ts
@@ -49,7 +49,7 @@ test.describe('e2e', () => {
     });
 
     test('script tag', async ({ page, packageName }) => {
-      if (packageName === 'e2e-solid') {
+      if (['e2e-solid', 'e2e-vue2', 'e2e-vue3', 'e2e-react'].includes(packageName)) {
         test.skip();
       }
 
