0b46390cccf71b3bef374fe41534a39ec903cb10
Sami Jaber <jabersami@gmail.com>
2023-06-22 12:00:29 -0300

    f

--- packages/core/src/__tests__/data/context/component-with-context.raw.tsx
+++ packages/core/src/__tests__/data/context/component-with-context.raw.tsx
@@ -1,19 +1,9 @@
-import { setContext, useContext, useMetadata } from '@builder.io/mitosis';
+import { setContext, useContext } from '@builder.io/mitosis';
 import Context1 from '@dummy/1';
 import Context2 from '@dummy/2';
-import ReactiveContext1 from '@dummy/3';
-import ReactiveContext2 from '@dummy/4';
-
-useMetadata({
-  context: {
-    ReactiveContext1: 'reactive',
-    ReactiveContext2: 'reactive',
-  },
-});
 
 export default function ComponentWithContext(props: { content: string }) {
   const foo = useContext(Context1);
-  const foo2 = useContext(ReactiveContext1);
 
   setContext(Context1, {
     foo: 'bar',
@@ -22,19 +12,9 @@ export default function ComponentWithContext(props: { content: string }) {
     },
   });
 
-  setContext(ReactiveContext1, {
-    foo: 'bar',
-    content() {
-      return props.content;
-    },
-  });
-
   return (
     <Context2.Provider value={{ bar: 'baz' }}>
-      <ReactiveContext2.Provider value={{ bar: 'baz' }}>
-        <>{foo.value}</>
-        <>{foo2.value}</>
-      </ReactiveContext2.Provider>
+      <>{foo.value}</>
     </Context2.Provider>
   );
 }
