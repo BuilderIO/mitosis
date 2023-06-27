4f47a9de277d26218e48bb74f306859240610e63
Sami Jaber <jabersami@gmail.com>
2023-06-27 16:22:06 -0300

    f

--- packages/core/src/generators/svelte/helpers.ts
+++ packages/core/src/generators/svelte/helpers.ts
@@ -1,16 +1,7 @@
-import { types } from '@babel/core';
-import { processSignalsForCode } from '../../helpers/plugins/process-signals';
 import { isSlotProperty, replaceSlotsInString } from '../../helpers/slots';
 import { stripStateAndPropsRefs } from '../../helpers/strip-state-and-props-refs';
 import { MitosisComponent } from '../../types/mitosis-component';
 import { ToSvelteOptions } from './types';
-
-export const transformReactiveValues = ({ json }: { json: MitosisComponent }) => {
-  return processSignalsForCode({
-    json,
-    mapSignal: (name) => types.identifier('$' + name),
-  });
-};
 
 export const stripStateAndProps =
   ({ options, json }: { options: ToSvelteOptions; json: MitosisComponent }) =>
