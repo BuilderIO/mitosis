58c709c9affb8b21159a53185c986a4f46dabe48
Sami Jaber <jabersami@gmail.com>
2023-07-11 21:33:35 -0300

    k

--- packages/core/src/__tests__/data/context-state.raw.tsx
+++ packages/core/src/__tests__/data/context-state.raw.tsx
@@ -5,6 +5,14 @@ export default function RenderContent(props) {
   setContext(BuilderContext, {
     content: props.content,
     registeredComponents: props.customComponents,
+    foo: {
+      bar: 'baz',
+      baz: 123,
+      fn: () => {
+        const a = 1;
+        return 'hello' + 'world' + a;
+      },
+    },
   });
 
   return <div>setting context</div>;
