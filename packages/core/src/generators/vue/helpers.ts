2a69fe36b8e3a137b973b17c2ca14ab2d4d15e0b
Sami Jaber <jabersami@gmail.com>
2023-07-18 16:58:22 -0300

    fix

--- packages/core/src/generators/vue/helpers.ts
+++ packages/core/src/generators/vue/helpers.ts
@@ -192,16 +192,17 @@ export const processBinding = ({
         switch (options.api) {
           // keep pointing to `props.${value}`
           case 'composition':
-            if (codeType === 'bindings') {
-              return isSlotProperty(name) ? replaceSlotsInString(name, (x) => `$slots.${x}`) : name;
-            }
+            const slotPrefix = codeType === 'bindings' ? '$slots' : 'useSlots()';
 
             if (name === 'children' || name.startsWith('children.')) {
-              return `useSlots().default`;
+              return `${slotPrefix}.default`;
             }
             return isSlotProperty(name)
-              ? replaceSlotsInString(name, (x) => `useSlots().${x}`)
+              ? replaceSlotsInString(name, (x) => `${slotPrefix}.${x}`)
+              : codeType === 'bindings'
+              ? name
               : `props.${name}`;
+
           case 'options':
             return optionsApiStateAndPropsReplace(name, thisPrefix, codeType);
         }
