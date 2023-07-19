4d1eae564114eb2bd1d03a2d3ef4d9f7791b7dd4
Sami Jaber <jabersami@gmail.com>
2023-07-19 16:41:31 -0300

    Fix/target properties (#1229)
    * fix
    
    * update
    
    * release
    
    * fix
    
    * fix
    
    * k
    
    * fix
    
    * f
    
    * f
    
    * fix test
    
    * f

--- packages/core/src/__tests__/data/styles/style-prop-class-and-css.raw.tsx
+++ packages/core/src/__tests__/data/styles/style-prop-class-and-css.raw.tsx
@@ -1,8 +1,15 @@
+import { useTarget } from '@builder.io/mitosis';
+
 export default function StylePropClassAndCss(props) {
   return (
     <div
       style={props.attributes.style}
-      class="builder-column"
+      class={useTarget({
+        vue3: props.attributes.className,
+        vue2: props.attributes.className,
+        svelte: props.attributes.classfdsa,
+        default: props.attributes.class,
+      })}
       css={{
         display: 'flex',
         flexDirection: 'column',
--- packages/core/src/helpers/plugins/process-code/types.ts
+++ packages/core/src/helpers/plugins/process-code/types.ts
@@ -1,4 +1,5 @@
 import { MitosisComponent } from '../../../types/mitosis-component';
+import { MitosisNode } from '../../../types/mitosis-node';
 
 export type CodeType =
   | 'hooks'
@@ -18,6 +19,7 @@ export type CodeType =
 declare function codeProcessor(
   codeType: CodeType,
   json: MitosisComponent,
-): (code: string, hookType: string) => string;
+  node?: MitosisNode,
+): (code: string, hookType: string) => string | (() => void);
 
 export type CodeProcessor = typeof codeProcessor;
