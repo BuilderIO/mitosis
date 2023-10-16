34be0f911f73c90a54ade90b97695c02f34ce1c1
Sami Jaber <jabersami@gmail.com>
2023-10-16 12:48:28 -0300

    rm

--- packages/site/src/components/TextLink.tsx
+++
@@ -1,6 +0,0 @@
-import { AnchorHTMLAttributes } from 'react';
-import { colors } from '../constants/colors';
-
-export function TextLink(props: AnchorHTMLAttributes<HTMLAnchorElement>) {
-  return <a rel="noreferrer" css={{ color: colors.primary, cursor: 'pointer' }} {...props} />;
-}
