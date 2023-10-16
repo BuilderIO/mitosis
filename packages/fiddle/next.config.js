704bf11b4ee3daa02b4f7b93bd82ec325977dfba
Sami Jaber <jabersami@gmail.com>
2023-10-16 17:08:05 -0300

    f

--- packages/fiddle/next.config.js
+++ packages/fiddle/next.config.js
@@ -4,21 +4,21 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
  */
 const nextConfig = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      // @babel/core imported by Mitosis imports `fs`. We don't need it in the browser, so we tell webpack to ignore it.
-      // https://webpack.js.org/configuration/resolve/#resolvealias
-      fs: false,
-      module: false,
-    };
     config.resolve.mainFields = ['browser', 'main', 'module'];
 
     config.resolve.plugins = [...config.resolve.plugins, new TsconfigPathsPlugin()];
 
+    /**
+     * These node pkgs areimported by Mitosis, but we don't need them in the browser, so we tell webpack to ignore it.
+     * https://webpack.js.org/configuration/resolve/#resolvealias
+     */
     const falseAliases = {
       'globby': false,
       'coffeescript': false,
       'postcss-load-config': false,
+      'perf_hooks': false,
+      fs: false,
+      module: false,
       'CLIEngine': false,
       'svelte-preprocess': false,
       sass: false
