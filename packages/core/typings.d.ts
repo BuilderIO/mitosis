c1d76fd4225bc854fe9cfd94e26e44c593e186b9
Sami Jaber <jabersami@gmail.com>
2023-10-17 11:00:10 -0300

    fmt

--- packages/core/typings.d.ts
+++ packages/core/typings.d.ts
@@ -9,4 +9,4 @@ declare module '@babel/plugin-syntax-decorators';
 declare module '@babel/plugin-syntax-typescript';
 declare module '@babel/preset-typescript';
 
-declare module 'prettier-plugin-svelte'
+declare module 'prettier-plugin-svelte';
