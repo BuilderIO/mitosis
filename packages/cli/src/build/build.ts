a6796b7cced22e0a2303dbcf59a87458f9cbfb6e
Sami Jaber <jabersami@gmail.com>
2023-10-27 19:03:20 -0300

    Fix commonOptions.ts config (#1285)
    * fix commonOptions.ts config
    
    * f
    
    * f

--- packages/cli/src/build/build.ts
+++ packages/cli/src/build/build.ts
@@ -41,6 +41,7 @@ import { kebabCase } from 'lodash';
 import { fastClone } from '../helpers/fast-clone';
 import { generateContextFile } from './helpers/context';
 import { getFiles } from './helpers/files';
+import { checkIsDefined } from './helpers/nullable';
 import { getOverrideFile } from './helpers/overrides';
 import { transformImports, transpile, transpileIfNecessary } from './helpers/transpile';
 
@@ -69,14 +70,26 @@ const DEFAULT_CONFIG: Partial<MitosisConfig> = {
   getTargetPath,
 };
 
-const getOptions = (config?: MitosisConfig): MitosisConfig => ({
-  ...DEFAULT_CONFIG,
-  ...config,
-  options: {
-    ...DEFAULT_CONFIG.options,
-    ...config?.options,
-  },
-});
+const getOptions = (config?: MitosisConfig): MitosisConfig => {
+  const newConfig = {
+    ...DEFAULT_CONFIG,
+    ...config,
+    options: {
+      ...DEFAULT_CONFIG.options,
+      ...config?.options,
+    },
+  };
+
+  if (checkIsDefined(newConfig.commonOptions?.typescript)) {
+    for (const target of newConfig.targets) {
+      if (!checkIsDefined(newConfig.options[target].typescript)) {
+        newConfig.options[target].typescript = newConfig.commonOptions.typescript;
+      }
+    }
+  }
+
+  return newConfig;
+};
 
 async function clean(options: MitosisConfig, target: Target) {
   const outputPattern = `${options.dest}/${options.getTargetPath({ target })}/${options.files}`;
@@ -118,9 +131,7 @@ const getRequiredParsers = (
 ): { javascript: boolean; typescript: boolean } => {
   const targetsOptions = Object.values(options.options);
 
-  const targetsRequiringTypeScript = targetsOptions.filter(
-    (option) => option.typescript || options.commonOptions?.typescript,
-  ).length;
+  const targetsRequiringTypeScript = targetsOptions.filter((option) => option.typescript).length;
   const needsTypeScript = targetsRequiringTypeScript > 0;
 
   /**
