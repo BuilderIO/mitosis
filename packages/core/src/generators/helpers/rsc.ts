34062410e6bfe4dd8003cd058722f9dc24b98176
Sami Jaber <jabersami@gmail.com>
2023-10-31 16:38:24 -0300

    Fix: RSC check (#1287)
    * fix commonOptions.ts config
    
    * f
    
    * f
    
    * on-event-arg
    
    * f
    
    * fix
    
    * f
    
    * f
    
    * f
    
    * improve sfds
    
    * rl
    
    * rlrl
    
    * fix and move rsc logic
    
    * f
    
    * f
    
    * fix

---
+++ packages/core/src/generators/helpers/rsc.ts
@@ -0,0 +1,29 @@
+import traverse from 'traverse';
+import { isMitosisNode } from '../../helpers/is-mitosis-node';
+import type { MitosisComponent } from '../../types/mitosis-component';
+import type { MitosisNode } from '../../types/mitosis-node';
+
+const checkIsNodeAMitosisComponent = (node: MitosisNode) =>
+  node.name[0] === node.name[0].toUpperCase();
+
+export const checkIfIsClientComponent = (json: MitosisComponent) => {
+  if (json.hooks.onMount.length) return true;
+  if (json.hooks.onUnMount?.code) return true;
+  if (json.hooks.onUpdate?.length) return true;
+  if (Object.keys(json.refs).length) return true;
+  if (Object.keys(json.context.set).length) return true;
+  if (Object.keys(json.context.get).length) return true;
+  if (Object.values(json.state).filter((s) => s?.type === 'property').length) return true;
+
+  let foundEventListener = false;
+  traverse(json).forEach(function (node) {
+    if (isMitosisNode(node) && !checkIsNodeAMitosisComponent(node)) {
+      if (Object.keys(node.bindings).filter((item) => item.startsWith('on')).length) {
+        foundEventListener = true;
+        this.stop();
+      }
+    }
+  });
+
+  return foundEventListener;
+};
--- packages/core/src/generators/react/generator.ts
+++ packages/core/src/generators/react/generator.ts
@@ -37,8 +37,8 @@ import {
 import { MitosisComponent } from '../../types/mitosis-component';
 import { TranspilerGenerator } from '../../types/transpiler';
 import { hasContext } from '../helpers/context';
+import { checkIfIsClientComponent } from '../helpers/rsc';
 import { collectReactNativeStyles } from '../react-native';
-import { checkIfIsClientComponent } from '../rsc';
 import { blockToReact } from './blocks';
 import { closeFrag, getCode, openFrag, processTagReferences, wrapInFragment } from './helpers';
 import { getUseStateCode, processHookCode, updateStateSetters } from './state';
