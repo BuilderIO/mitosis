2138014773053bc92e0ea4d7825330c2f6a6aa74
Sami Jaber <jabersami@gmail.com>
2023-11-02 16:46:27 -0300

    extend timeout

--- packages/cli/src/__tests__/cli-integration.test.ts
+++ packages/cli/src/__tests__/cli-integration.test.ts
@@ -4,49 +4,66 @@ import { expect, test } from 'vitest';
 
 const { version } = require('../../package.json');
 
-const root = filesystem.path(__dirname, '..', '..');
-const script = filesystem.path(root, 'bin', 'mitosis');
-
 const cli = async (cmd: string) => {
-  const shcmd = `node ${script} ${cmd}`;
+  const root = filesystem.path(__dirname, '..', '..');
+  const mitosisCliScript = filesystem.path(root, 'bin', 'mitosis');
+  const shcmd = `node ${mitosisCliScript} ${cmd}`;
   console.debug(`Running: ${shcmd}`);
   return system.run(shcmd);
 };
 
-test('outputs version', async () => {
-  const output = await cli('--version');
-  expect(output).toContain(version);
-});
+const TIMEOUT = 10000;
 
-test('outputs help', async () => {
-  const output = await cli('--help');
-  expect(output).toContain(version);
-});
+test(
+  'outputs version',
+  async () => {
+    const output = await cli('--version');
+    expect(output).toContain(version);
+  },
+  { timeout: TIMEOUT },
+);
+
+test(
+  'outputs help',
+  async () => {
+    const output = await cli('--help');
+    expect(output).toContain(version);
+  },
+  { timeout: TIMEOUT },
+);
 
 // TODO refactor commands/compile.ts to not have side effects (like calling
 // process.exit) so that this can be unit tested instead.
-test('strips out builder components by default', async () => {
-  const filepath = path.resolve(__dirname, 'data/triptych.builder.json');
+test(
+  'strips out builder components by default',
+  async () => {
+    const filepath = path.resolve(__dirname, 'data/triptych.builder.json');
 
-  const output = await cli(`compile --from=builder --to=react ${filepath}`);
+    const output = await cli(`compile --from=builder --to=react ${filepath}`);
 
-  expect(output).toMatchSnapshot();
-  expect(output).toContain('function MyComponent(props) {');
-  expect(output).not.toContain('<Columns');
-  expect(output).not.toContain('<Column');
-  expect(output).not.toContain('<Image');
-  expect(output).toContain('<img');
-});
+    expect(output).toMatchSnapshot();
+    expect(output).toContain('function MyComponent(props) {');
+    expect(output).not.toContain('<Columns');
+    expect(output).not.toContain('<Column');
+    expect(output).not.toContain('<Image');
+    expect(output).toContain('<img');
+  },
+  { timeout: TIMEOUT },
+);
 
-test('--builder-components keeps builder components', async () => {
-  const filepath = path.resolve(__dirname, 'data/triptych.builder.json');
+test(
+  '--builder-components keeps builder components',
+  async () => {
+    const filepath = path.resolve(__dirname, 'data/triptych.builder.json');
 
-  const output = await cli(`compile --builder-components --from=builder --to=react ${filepath}`);
+    const output = await cli(`compile --builder-components --from=builder --to=react ${filepath}`);
 
-  expect(output).toMatchSnapshot();
-  expect(output).toContain('function MyComponent(props) {');
-  expect(output).toContain('<Columns');
-  expect(output).toContain('<Column');
-  expect(output).toContain('<Image');
-  expect(output).not.toContain('<img');
-});
+    expect(output).toMatchSnapshot();
+    expect(output).toContain('function MyComponent(props) {');
+    expect(output).toContain('<Columns');
+    expect(output).toContain('<Column');
+    expect(output).toContain('<Image');
+    expect(output).not.toContain('<img');
+  },
+  { timeout: TIMEOUT },
+);
