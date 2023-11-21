62351a2ed7a8fb528e3d68c8a6154ec42c0f69f6
Sami Jaber <jabersami@gmail.com>
2023-11-21 18:37:28 -0400

    bye

--- packages/starter/template/library/src/todo-app/todo-app.lite.tsx
+++ packages/starter/template/library/src/todo-app/todo-app.lite.tsx
@@ -1,6 +1,4 @@
-import { useStore } from '@builder.io/mitosis';
-
-import ItemList from './item-list.lite';
+import { For, useStore } from '@builder.io/mitosis';
 
 export default function TodoApp() {
   const state = useStore({
@@ -64,7 +62,49 @@ export default function TodoApp() {
           marginTop: '1rem',
         }}
       >
-        <ItemList list={state.list} deleteItem={(i) => state.deleteItem(i)}></ItemList>
+        <ul
+          css={{
+            borderRadius: '0.25rem',
+            boxShadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+            margin: 'unset',
+            padding: 'unset',
+          }}
+        >
+          <For each={state.list}>
+            {(item, idx) => (
+              <li
+                key={idx}
+                css={{
+                  display: 'flex',
+                  padding: '0.625rem',
+                  alignItems: 'center',
+                  borderBottomWidth: '1px',
+                  borderColor: '#E5E7EB',
+                  gap: '16px',
+                }}
+              >
+                <span>{item}</span>
+                <button
+                  css={{
+                    cursor: 'pointer',
+                    paddingTop: '0.5rem',
+                    paddingBottom: '0.5rem',
+                    paddingLeft: '1rem',
+                    paddingRight: '1rem',
+                    borderRadius: '0.25rem',
+                    color: '#ffffff',
+                    backgroundColor: '#EF4444',
+                  }}
+                  onClick={() => {
+                    state.deleteItem(idx);
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            )}
+          </For>
+        </ul>
       </div>
     </div>
   );
