684e2eba33c8d062ee3884a19e9c35f01035b4b8
Sami Jaber <jabersami@gmail.com>
2023-11-21 16:56:09 -0400

    lint

--- packages/starter/template/library/packages/qwik/vite.config.ts
+++ packages/starter/template/library/packages/qwik/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import pkg from "./package.json";
-import { qwikVite } from "@builder.io/qwik/optimizer";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { qwikVite } from '@builder.io/qwik/optimizer';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import pkg from './package.json';
 
 const { dependencies = {}, peerDependencies = {} } = pkg as any;
 const makeRegex = (dep) => new RegExp(`^${dep}(/.*)?$`);
@@ -10,19 +10,15 @@ const excludeAll = (obj) => Object.keys(obj).map(makeRegex);
 export default defineConfig(() => {
   return {
     build: {
-      target: "es2020",
+      target: 'es2020',
       lib: {
-        entry: "./src/index.ts",
-        formats: ["es", "cjs"],
-        fileName: (format) => `index.qwik.${format === "es" ? "mjs" : "cjs"}`,
+        entry: './src/index.ts',
+        formats: ['es', 'cjs'],
+        fileName: (format) => `index.qwik.${format === 'es' ? 'mjs' : 'cjs'}`,
       },
       rollupOptions: {
         // externalize deps that shouldn't be bundled into the library
-        external: [
-          /^node:.*/,
-          ...excludeAll(dependencies),
-          ...excludeAll(peerDependencies),
-        ],
+        external: [/^node:.*/, ...excludeAll(dependencies), ...excludeAll(peerDependencies)],
       },
     },
     plugins: [qwikVite(), tsconfigPaths()],
