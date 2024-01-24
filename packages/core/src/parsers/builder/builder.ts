3569a7b28dadafe3cbe6680c556ce9dbfc434d21
Korey Kassir <korey@builder.io>
2024-01-24 12:53:25 -0800

    feat(mitosis): code gen support for named slots (#1334)
    * feat(mitosis): code gen support for named slots
    
    * pr feedback
    
    * fix type change
    
    * fix type change
    
    * pr feedback
    
    * update readme

--- packages/core/src/parsers/builder/builder.ts
+++ packages/core/src/parsers/builder/builder.ts
@@ -577,6 +577,7 @@ export const builderElementToMitosisNode = (
   }
 
   const bindings: any = {};
+  const children: MitosisNode[] = [];
 
   if (blockBindings) {
     for (const key in blockBindings) {
@@ -617,8 +618,28 @@ export const builderElementToMitosisNode = (
   if (block.component?.options) {
     for (const key in block.component.options) {
       const value = block.component.options[key];
+      const valueIsArrayOfBuilderElements = Array.isArray(value) && value.every(isBuilderElement);
+
       if (typeof value === 'string') {
         properties[key] = value;
+      } else if (valueIsArrayOfBuilderElements) {
+        const childrenElements = value
+          .filter((item) => {
+            if (item.properties?.src?.includes('/api/v1/pixel')) {
+              return false;
+            }
+            return true;
+          })
+          .map((item) => builderElementToMitosisNode(item, options));
+        children.push({
+          '@type': '@builder.io/mitosis/node',
+          name: 'Slot',
+          meta: {},
+          scope: {},
+          bindings: {},
+          properties: { name: key },
+          children: childrenElements,
+        });
       } else {
         bindings[key] = { code: json5.stringify(value) };
       }
@@ -695,7 +716,9 @@ export const builderElementToMitosisNode = (
     }
   }
 
-  node.children = (block.children || []).map((item) => builderElementToMitosisNode(item, options));
+  node.children = children.concat(
+    (block.children || []).map((item) => builderElementToMitosisNode(item, options)),
+  );
 
   return node;
 };
