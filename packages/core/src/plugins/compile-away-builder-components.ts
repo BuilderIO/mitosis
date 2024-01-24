9b767ea63e12def3ee6014c836f606bc40b9cbbc
Manu MA <manu.mtza@gmail.com>
2024-01-24 08:51:46 +0100

    fix: less css for compiled away columns

--- packages/core/src/plugins/compile-away-builder-components.ts
+++ packages/core/src/plugins/compile-away-builder-components.ts
@@ -276,6 +276,7 @@ export const components: CompileAwayComponentsMap = {
           css: createSingleBinding({
             code: JSON.stringify({
               gap: `${gutterSize}px`,
+              alignItems: 'stretch',
               display: 'flex',
               ...(properties.stackColumnsAt === 'never'
                 ? {}
@@ -286,8 +287,6 @@ export const components: CompileAwayComponentsMap = {
                           properties.reverseColumnsWhenStacked === 'true'
                             ? 'column-reverse'
                             : 'column',
-                        alignItems: 'stretch',
-                        gap: `0px`,
                       },
                   }),
             }),
@@ -305,10 +304,7 @@ export const components: CompileAwayComponentsMap = {
                   display: 'flex',
                   flexDirection: 'column',
                   alignItems: 'stretch',
-                  lineHeight: 'normal',
-
                   width: `${getColumnWidth(index)}`,
-                  marginLeft: `${index === 0 ? 0 : gutterSize}px`,
                   ...(properties.stackColumnsAt === 'never'
                     ? {}
                     : {
@@ -316,7 +312,6 @@ export const components: CompileAwayComponentsMap = {
                           properties.stackColumnsAt === 'mobile' ? 640 : 991
                         }px)`]: {
                           width: '100%',
-                          marginLeft: 0,
                         },
                       }),
                 }),
