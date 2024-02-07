34f2fe1b2ed20971815c55687b68aaa033a052ab
Sami Jaber <jabersami@gmail.com>
2024-02-07 16:33:36 -0400

    rev

--- packages/core/src/__tests__/react.test.ts
+++ packages/core/src/__tests__/react.test.ts
@@ -1,6 +1,26 @@
+import { parseJsx } from '..';
 import { componentToReact } from '../generators/react';
 import { runTestsForTarget } from './test-generator';
 
+import stamped from './data/blocks/stamped-io.raw.tsx?raw';
+
 describe('React', () => {
   runTestsForTarget({ options: {}, target: 'react', generator: componentToReact });
+  test('stamped (useState)', () => {
+    const component = parseJsx(stamped);
+    const output = componentToReact({
+      stylesType: 'style-tag',
+      stateType: 'useState',
+    })({ component });
+    expect(output).toMatchSnapshot();
+  });
+
+  test('stamped (mobx)', () => {
+    const component = parseJsx(stamped);
+    const output = componentToReact({
+      stylesType: 'style-tag',
+      stateType: 'mobx',
+    })({ component });
+    expect(output).toMatchSnapshot();
+  });
 });
