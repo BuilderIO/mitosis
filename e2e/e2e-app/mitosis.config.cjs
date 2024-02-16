d840862b6422ee5f8d153301feb2cbe55ce2f78d
Sami Jaber <jabersami@gmail.com>
2024-02-16 13:11:09 -0400

    yeet

--- e2e/e2e-app/mitosis.config.cjs
+++ e2e/e2e-app/mitosis.config.cjs
@@ -1,24 +1,3 @@
-/**
- * @param {string} string
- */
-const kebabCase = (string) => string.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
-
-/**
- * @type {MitosisConfig['getTargetPath']}
- */
-const getTargetPath = ({ target }) => {
-  switch (target) {
-    // we have to workaround a name collision, where the folder can't have the name of the `exports` property in package.json.
-    // crazy, crazy stuff.
-    case 'vue2':
-      return 'vue/packages/_vue2';
-    case 'vue':
-    case 'vue3':
-      return 'vue/packages/_vue3';
-    default:
-      return kebabCase(target);
-  }
-};
 const vueConfig = {
   transpiler: { format: 'esm' },
   asyncComponentImports: true,
@@ -39,17 +18,14 @@ module.exports = {
     'reactNative',
     'solid',
     'svelte',
-    'vue2',
-    'vue3',
+    'vue',
     'webcomponent',
   ],
   options: {
     react: { transpiler: { format: 'esm', languages: ['ts'] }, typescript: true },
     solid: { transpiler: { languages: ['ts'] }, typescript: true },
-    vue2: vueConfig,
-    vue3: { ...vueConfig, api: 'composition' },
+    vue: { ...vueConfig, api: 'composition' },
     qwik: { typescript: true },
     svelte: { typescript: true },
   },
-  getTargetPath,
 };
--- e2e/e2e-vue2/README.md
+++
@@ -1,9 +0,0 @@
-# E2E test harness for Vue 2
-
-To recompile and run just this set of E2E tests, you can run these commands at
-the project root:
-
-```bash
-yarn workspace @builder.io/e2e-vue2 run build
-yarn workspace @builder.io/e2e-vue2 run e2e
-```
