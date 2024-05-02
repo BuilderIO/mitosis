c7169848108510c789f2b5db3e0f7cd3adf97e7a
Steve Sewell <steve@builder.io>
2024-05-02 09:34:17 -0700

    buttons

--- packages/docs/src/routes/index.tsx
+++ packages/docs/src/routes/index.tsx
@@ -12,13 +12,13 @@ export default component$(() => {
       <div class="flex mx-auto mt-12">
         <Link
           href="/docs/quickstart"
-          class="btn bg-neutral-500 hover:bg-neutral-700 text-white font-bold py-2 px-4 rounded"
+          class="btn bg-neutral-600 hover:bg-neutral-500 text-white font-bold py-2 px-4 rounded"
         >
           Get Started
         </Link>
         <Link
           href="/playground"
-          class="btn ml-4 border border-neutral-500 hover:bg-neutral-700 text-white font-bold py-2 px-4 rounded"
+          class="btn ml-4 border border-neutral-600 hover:bg-neutral-700 text-white font-bold py-2 px-4 rounded"
         >
           Try the Playground
         </Link>
