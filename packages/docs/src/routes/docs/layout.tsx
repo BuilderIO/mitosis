4005d5bdbb2a30356baf2950a9cc66a09d1879c2
Steve Sewell <steve@builder.io>
2024-05-03 08:00:39 -0700

    pretty up

--- packages/docs/src/routes/docs/layout.tsx
+++ packages/docs/src/routes/docs/layout.tsx
@@ -3,8 +3,8 @@ import Sidebar from './sidebar';
 
 export default component$(() => {
   return (
-    <div class="flex items-start max-md:flex-col-reverse">
-      <Sidebar class="sticky top-20" />
+    <div class="flex items-start max-md:flex-col-reverse gap-8 mt-8 max-md:mt-2">
+      <Sidebar class="sticky top-20 mt-24 max-md:mt-4" />
       <div class="prose prose-invert p-8 max-md:px-4 lg:prose-xl min-w-0 max-w-full">
         <Slot />
       </div>
