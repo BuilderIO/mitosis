c737e954ba1afdfcafa77b0d5200756fc38fa5b8
Steve Sewell <steve@builder.io>
2024-05-03 08:22:44 -0700

    styling

--- packages/docs/src/components/header.tsx
+++ packages/docs/src/components/header.tsx
@@ -5,6 +5,7 @@ export default component$(() => {
   const location = useLocation();
 
   const isPlayground = location.url.pathname === '/playground/';
+  const isDocs = location.url.pathname.startsWith('/docs/');
 
   return (
     <>
@@ -13,28 +14,37 @@ export default component$(() => {
       </div>
       <header class="text-white sticky top-0 z-10 border-b border-primary border-opacity-50 bg-purple-990">
         <div
-          class={['p-6 flex justify-between items-center', !isPlayground && 'container mx-auto']}
+          class={[
+            'p-6 max-md:p-4 flex justify-between items-center',
+            !isPlayground && 'container mx-auto',
+          ]}
         >
           <a href="/" title="qwik" class="flex items-center">
             <img
-              class="object-contain"
+              class="object-contain max-md:max-w-[110px]"
               width={140}
               height={50}
               src="https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F0fdb9aabd10f4205b3b3b56d7b950239"
             />
           </a>
-          <ul class="flex space-x-8 font-medium">
+          <ul class="flex space-x-8 max-md:space-x-6 font-medium">
             <li>
-              <Link href="/docs" class="hover:underline">
+              <Link
+                href="/docs"
+                class={['hover:text-primary-light', isDocs && 'text-primary-light']}
+              >
                 Docs
               </Link>
             </li>
             <li>
-              <Link href="/playground" class="hover:underline">
+              <Link
+                href="/playground"
+                class={['hover:text-primary-light', isPlayground && 'text-primary-light']}
+              >
                 Playground
               </Link>
             </li>
-            <li>
+            <li class="max-md:hidden">
               <Link
                 href="/docs/quickstart"
                 class="bg-primary rounded hover:bg-primary-light px-4 py-3 text-black"
