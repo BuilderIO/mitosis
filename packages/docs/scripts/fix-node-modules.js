86ad4d29edf67eb1da2489eabb84fec6fc518514
Steve Sewell <steve@builder.io>
2024-05-03 15:31:20 -0700

    fix?

--- packages/docs/scripts/fix-node-modules.js
+++ packages/docs/scripts/fix-node-modules.js
@@ -43,27 +43,10 @@ import("node:async_hooks").then((module) => {
     to: '',
   },
   // Prettier is a CJS module that can't be loaded from a "module" module,
-  // so we delete the import and then the usage of it in the next blockz
+  // so we delete the import
   {
     path: 'node_modules/@builder.io/qwik-labs-canary/vite/index.js',
     from: `import { format } from "prettier/standalone";`,
-    to: '',
-  },
-  {
-    path: 'node_modules/@builder.io/qwik-labs-canary/vite/index.js',
-    from: `
-    source = await format(source, {
-      parser: "typescript",
-      plugins: [
-        // To support running in browsers
-        require("prettier/plugins/estree"),
-        require("prettier/parser-typescript"),
-        require("prettier/parser-postcss"),
-        require("prettier/parser-html"),
-        require("prettier/parser-babel")
-      ],
-      htmlWhitespaceSensitivity: "ignore"
-    });`.trim(),
     to: '',
   },
 ];
