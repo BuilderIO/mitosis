276d7dfc882b3683f9dcc94673adc2bb75daca8d
Steve Sewell <steve@builder.io>
2024-05-03 16:52:16 -0700

    mobile footer

--- packages/docs/src/components/footer.tsx
+++ packages/docs/src/components/footer.tsx
@@ -4,13 +4,13 @@ import { TbBrandDiscord, TbBrandGithub } from '@qwikest/icons/tablericons';
 
 export default component$(() => {
   return (
-    <footer class="border-t border-primary border-opacity-50 pb-16">
-      <div class="container p-8 mt-8 mx-auto flex">
+    <footer class="border-t border-primary border-opacity-50 pb-4">
+      <div class="container p-8 md:my-8 mx-auto flex">
         <a href="https://www.builder.io/" target="_blank">
           <span>Made with ❤️</span>
 
           <img
-            class="object-contain mx-auto mt-3"
+            class="object-contain mx-auto mt-3 max-sm:w-[150px]"
             width={200}
             height={90}
             src="https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2Fe2052e9c2ef34dab98709a67fcc4b8c3"
@@ -25,7 +25,7 @@ export default component$(() => {
             <TbBrandGithub />
           </Link>
           <Link
-            class="hover:text-primary-light max-sm:hidden"
+            class="hover:text-primary-light"
             target="_blank"
             href="https://discord.com/invite/SNusEyNGsx"
           >
---
+++ packages/docs/src/components/search.tsx
@@ -0,0 +1,19 @@
+import { docsearch } from 'meilisearch-docsearch';
+import 'meilisearch-docsearch/css';
+
+import { component$, useSignal, useVisibleTask$ } from '@builder.io/qwik';
+
+export const Search = component$(() => {
+  const divRef = useSignal<HTMLDivElement>();
+
+  useVisibleTask$(() => {
+    docsearch({
+      container: divRef.value!,
+      host: "https://ms-0040a28198ad-9480.lon.meilisearch.io",
+      apiKey: "86cf79d6194eff5fe82ed4e5afc7d8135a29697572b9979c8dc8fc506fc58d1a", 
+      indexUid: "docs-site-crawl",
+    });
+  });
+
+  return <div ref={divRef} />;
+});
