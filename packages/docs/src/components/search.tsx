e80dcb685eb0e5a2552bfa92326309440ca2e6cc
Steve Sewell <steve@builder.io>
2024-05-03 18:00:02 -0700

    fuck ya

--- packages/docs/src/components/search.tsx
+++ packages/docs/src/components/search.tsx
@@ -10,6 +10,7 @@ export const Search = component$(() => {
 
   useVisibleTask$(() => {
     document.documentElement.setAttribute('data-theme', 'dark');
+    divRef.value!.innerHTML = '';
     docsearch({
       container: divRef.value!,
       host: 'https://ms-0040a28198ad-9480.lon.meilisearch.io',
@@ -29,5 +30,28 @@ export const Search = component$(() => {
     });
   });
 
-  return <div class="max-sm:hidden -my-2" id="searchbar" ref={divRef} />;
+  return (
+    <div class="max-sm:hidden -my-2" id="searchbar" ref={divRef}>
+      <button type="button" class="docsearch-btn" aria-label="Search">
+        <span class="docsearch-btn-icon-container">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            class="docsearch-modal-btn-icon"
+          >
+            <path
+              fill="currentColor"
+              d="M21.71 20.29L18 16.61A9 9 0 1 0 16.61 18l3.68 3.68a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.39ZM11 18a7 7 0 1 1 7-7a7 7 0 0 1-7 7Z"></path>
+          </svg>
+        </span>
+        <span class="docsearch-btn-placeholder"> Search</span>
+        <span class="docsearch-btn-keys">
+          <kbd class="docsearch-btn-key">⌘</kbd>
+          <kbd class="docsearch-btn-key">K</kbd>
+        </span>
+      </button>
+    </div>
+  );
 });
