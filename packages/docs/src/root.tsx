8d0a928a2cbab75d5e9cd5231159078a4aff7f50
Steve Sewell <steve@builder.io>
2024-05-05 07:12:50 -0700

    partytown

--- packages/docs/src/root.tsx
+++ packages/docs/src/root.tsx
@@ -1,5 +1,6 @@
 import { component$ } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+import { QwikPartytown } from './components/partytown/partytown';
 import { RouterHead } from './components/router-head';
 
 import './global.css';
@@ -22,6 +23,7 @@ export default component$(() => {
         <Insights publicApiKey="22gsbhtjcyv" />
         <RouterHead />
         <ServiceWorkerRegister />
+        <QwikPartytown forward={['dataLayer.push']} />
       </head>
       <body lang="en">
         <RouterOutlet />
