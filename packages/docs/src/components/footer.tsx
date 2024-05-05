35e43371b0a71d21226530c2c9829a54ba613dbd
Steve Sewell <steve@builder.io>
2024-05-05 08:13:07 -0700

    hearts

--- packages/docs/src/components/footer.tsx
+++ packages/docs/src/components/footer.tsx
@@ -9,8 +9,8 @@ export default component$(() => {
     <footer class="border-t border-primary border-opacity-50 pb-4">
       <div class="container md:my-8 p-8 mx-auto flex gap-4 max-md:flex-col-reverse">
         <div class="block max-md:mt-8">
-          <a class="block" href="https://www.builder.io/" target="_blank">
-            <span>Made with ❤️ by</span>
+          <a class="block text-primary-light" href="https://www.builder.io/" target="_blank">
+            <span>Made with <span class="text-fuchsia-300">❤️</span> by</span>
 
             <img
               alt="Builder.io logo"
@@ -20,7 +20,7 @@ export default component$(() => {
               src="https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2Fe2052e9c2ef34dab98709a67fcc4b8c3"
             />
           </a>
-          <div class="text-xs mt-4 opacity-60 font-light">Copyright © {year} Builder.io, Inc.</div>
+          <div class="text-xs mt-4 text-primary-light opacity-90 font-light">Copyright © {year} Builder.io, Inc.</div>
         </div>
         <div class="flex max-md:flex-col gap-8 md:ml-auto items-center text-3xl max-md:items-start max-md:mt-2">
           <div class="flex gap-4 text-base">
