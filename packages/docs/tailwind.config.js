1a6b09b8e79e8d3feff6d7fab12ad72022044f88
Steve Sewell <steve@builder.io>
2024-05-07 17:32:58 -0700

    add aspect ratios to images

--- packages/docs/tailwind.config.js
+++ packages/docs/tailwind.config.js
@@ -6,7 +6,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: '#AC7EF4',
