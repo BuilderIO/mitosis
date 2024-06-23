3fc074b5a9ffafb85691abf81c0ee98b9183504d
Steve Sewell <steve@builder.io>
2024-06-23 14:48:22 -0700

    check origin off

--- packages/docs/src/entry.express.tsx
+++ packages/docs/src/entry.express.tsx
@@ -11,13 +11,13 @@ import {
   createQwikCity,
   type PlatformNode,
 } from "@builder.io/qwik-city/middleware/node";
-import "dotenv/config";
 import qwikCityPlan from "@qwik-city-plan";
 import { manifest } from "@qwik-client-manifest";
-import render from "./entry.ssr";
+import "dotenv/config";
 import express from "express";
-import { fileURLToPath } from "node:url";
 import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+import render from "./entry.ssr";
 
 declare global {
   interface QwikCityPlatform extends PlatformNode {}
@@ -32,6 +32,7 @@ const PORT = process.env.PORT ?? 3000;
 
 // Create the Qwik City Node middleware
 const { router, notFound } = createQwikCity({
+  checkOrigin: false,
   render,
   qwikCityPlan,
   manifest,
