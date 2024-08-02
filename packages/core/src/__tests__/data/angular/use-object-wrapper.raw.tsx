b769e88886f93b94cceb5d4b667c0fe14b4c9a56
Lukasz.Jablonski <lukasz.jablonski@ardentcode.com>
2024-08-02 11:10:13 +0200

    update test case

--- packages/core/src/__tests__/data/angular/use-object-wrapper.raw.tsx
+++ packages/core/src/__tests__/data/angular/use-object-wrapper.raw.tsx
@@ -1,18 +1,42 @@
+import { useStore } from '@builder.io/mitosis';
+
 export default function MyComponent(props) {
-  return (
-    <div>
-      <CustomComponent
-        tooltipPlacementOptions={{
-          positions: ['left', 'bottom', 'bottom-left'],
-          offsets: {
-            left: { topOffset: 0, leftOffset: -24 },
-            bottom: { topOffset: 24, leftOffset: 0 },
-            'bottom-left': { topOffset: 24, leftOffset: 0 },
-          },
-        }}
-      >
-        test
-      </CustomComponent>
-    </div>
-  );
+    const state = useStore({
+        attributes: {id: 1},
+        attributes2: {id2: 1},
+        something: {id3: 1},
+    });
+
+    return (
+        <div>
+            <Comp
+                val1={{...state.attributes2}}
+                val2={{
+                    ...state.attributes,
+                    ...state.attributes2,
+                }}
+                val3={{
+                    ...state.something,
+                    anything: 'hello',
+                    hello: 'world',
+                }}
+                val4={{
+                    ...state.attributes,
+                    ...state.something,
+                    anything: [1, 2, 3],
+                    hello: 'hello',
+                    ...state.attributes2,
+                }}
+                val5={{
+                    ...state.attributes,
+                    ...state.something,
+                    anything: [1, 2, 3],
+                    anythingString: ['a', 'b', 'c'],
+                    hello: 'hello',
+                    ...props.spreadAttrs,
+                }}
+                val6={{anything: [1, 2, 3]}}
+            />
+        </div>
+    );
 }
