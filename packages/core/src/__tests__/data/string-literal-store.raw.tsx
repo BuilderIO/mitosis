7c8cfdb928f2ab6e2ca97c7a3923c2a9f7df01ac
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2024-08-26 09:46:27 -0400

    prettierignore

--- packages/core/src/__tests__/data/string-literal-store.raw.tsx
+++ packages/core/src/__tests__/data/string-literal-store.raw.tsx
@@ -1,6 +1,7 @@
 import { useStore } from '@builder.io/mitosis';
 
 export default function StringLiteralStore() {
+  /* prettier-ignore */
   const state = useStore({ "foo": 123 });
 
   return <div>{state.foo}</div>;
