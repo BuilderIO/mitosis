a2d3b2b07254bbf0523865b66420ba502ef1a24e
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2024-09-19 11:54:36 -0400

    add test

--- packages/core/src/__tests__/builder.test.ts
+++ packages/core/src/__tests__/builder.test.ts
@@ -413,6 +413,33 @@ describe('Builder', () => {
     });
     expect(mitosis).toMatchSnapshot();
   });
+
+  test.only('preserve cssCode when converting', () => {
+    const builderJson: BuilderContent = {
+      data: {
+        cssCode: dedent`
+        .foo {
+          background: green;
+        }
+        
+        .bar {
+          font-weight: bold;
+        }
+      `,
+        blocks: [],
+      },
+    };
+    const builderToMitosis = builderContentToMitosisComponent(builderJson);
+    expect(builderToMitosis.meta.cssCode).toMatchSnapshot();
+
+    const mitosisToBuilder = componentToBuilder()({ component: builderToMitosis })!;
+    expect(mitosisToBuilder.data!.cssCode).toMatchSnapshot();
+
+    const jsx = componentToMitosis(mitosisOptions)({
+      component: builderToMitosis,
+    });
+    expect(jsx).toMatchSnapshot();
+  });
 });
 
 const bindingJson = {
