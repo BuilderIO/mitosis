997f673da2d862253747ff8f6ba39b99ed175aee
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2025-02-27 15:55:10 +0100

    feat: add dependency handling for onUpdate hook (#1702)
    * feat: add dependency handling for onUpdate hook
    
    * chore: update snapshots
    fix: issue with e2e test
    
    * fix: skip broken e2e tests for angular and solid

---
+++ e2e/e2e-app/src/components/component-on-update.lite.tsx
@@ -0,0 +1,51 @@
+import { onMount, onUpdate, useRef, useStore } from '@builder.io/mitosis';
+
+type Store = { initial: boolean; counter: number; label?: string; handleClick: () => void };
+
+export default function ComponentOnUpdate(props: any) {
+  const _ref = useRef<HTMLDivElement | null>(null);
+
+  const state = useStore<Store>({
+    initial: false,
+    counter: 0,
+    label: undefined,
+    handleClick: () => {
+      state.counter = state.counter + 1;
+    },
+  });
+
+  onMount(() => {
+    state.initial = true;
+  });
+
+  onUpdate(() => {
+    if (_ref && !state.initial) {
+      state.initial = true;
+    }
+  }, [_ref, state.initial]);
+
+  onUpdate(() => {
+    console.log(state.counter, state.label, state.initial);
+  });
+
+  onUpdate(() => {
+    if (state.initial) {
+      state.label = 'Label';
+    }
+  }, [state.initial]);
+
+  onUpdate(() => {
+    if (_ref && state.label) {
+      _ref.setAttribute('aria-label', `${state.label}: ${state.counter}`);
+    }
+  }, [_ref, state.counter, state.label]);
+
+  return (
+    <div ref={_ref} data-testid="container">
+      Hello {state.counter}
+      <button data-testid="button" onClick={() => state.handleClick()}>
+        Increase
+      </button>
+    </div>
+  );
+}
