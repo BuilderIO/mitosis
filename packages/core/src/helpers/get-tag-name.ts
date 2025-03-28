5717169e3e8a31408d93d0c10dc848b66decdb51
Sami Jaber <jabersami@gmail.com>
2025-03-28 15:37:48 -0300

    🏃

--- packages/core/src/helpers/get-tag-name.ts
+++ packages/core/src/helpers/get-tag-name.ts
@@ -1,4 +1,4 @@
-import { MitosisNode } from '..';
+import { MitosisNode } from '@/types/mitosis-node';
 
 export const getBuilderTagName = (node: MitosisNode) => {
   return node.properties.$tagName;
