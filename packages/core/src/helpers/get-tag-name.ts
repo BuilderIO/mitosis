db98671a87be5344959650f128d3e47d8b3e35f1
Sami Jaber <jabersami@gmail.com>
2025-03-28 15:37:31 -0300

    👹

--- packages/core/src/helpers/get-tag-name.ts
+++ packages/core/src/helpers/get-tag-name.ts
@@ -1,9 +1,5 @@
 import { MitosisNode } from '..';
 
-/**
- * Returns the tag name of the component. Checks for Builder's `$tagName`
- * property first, then falls back to the component name.
- */
 export const getBuilderTagName = (node: MitosisNode) => {
   return node.properties.$tagName;
 };
