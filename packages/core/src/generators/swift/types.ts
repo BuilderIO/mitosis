dc3de1e235706f980f9449814e89f83c228e1674
Kyle Fowler <kfowler@builder.io>
2025-04-25 15:45:12 -0500

    updated swift (#1755)
    * updated swift
    
    * we return an empty object which creates empty test suites
    
    * remove old
    
    * format the codes
    
    * changeset

--- packages/core/src/generators/swift/types.ts
+++ packages/core/src/generators/swift/types.ts
@@ -1,5 +1,35 @@
 import { BaseTranspilerOptions } from '@/types/transpiler';
 
-export interface ToSwiftOptions extends BaseTranspilerOptions {}
+export interface ToSwiftOptions extends BaseTranspilerOptions {
+  /**
+   * Format generated Swift code
+   * @default true
+   */
+  formatCode?: boolean;
+
+  /**
+   * Include type annotations in Swift code
+   * @default true
+   */
+  includeTypes?: boolean;
+
+  /**
+   * Type of state management to use
+   * @default 'state'
+   */
+  stateType?: 'state' | 'stateObject' | 'observable';
+
+  /**
+   * Prefix for class names
+   * @default ''
+   */
+  classPrefix?: string;
+
+  /**
+   * Whether to include SwiftUI preview code
+   * @default true
+   */
+  includePreview?: boolean;
+}
 
 export type SwiftMetadata = {};
